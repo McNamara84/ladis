0abbb4ed78abb509cb367b66a6499545e4d4c580
McNamara <holgerehrmann@gmail.com>
2025-05-31 17:12:44 +0200

    Added Bootstrap CSS and JS fallback for test environments

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -15,7 +15,13 @@
     <link href="https://fonts.bunny.net/css?family=Nunito:400,600,700" rel="stylesheet">
 
     <!-- Scripts -->
-    @vite(['resources/sass/app.scss', 'resources/js/app.js'])
+    @if (app()->environment('testing'))
+        <!-- Bootstrap CSS from CDN (für Tests) -->
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
+            integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    @else
+        @vite(['resources/sass/app.scss', 'resources/js/app.js'])
+    @endif
 </head>
 
 <body class="bg-light">
@@ -129,6 +135,13 @@
             </div>
         </footer>
     </div>
+
+    @if (app()->environment('testing'))
+        <!-- Bootstrap JS from CDN (for Tests) -->
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
+            integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL"
+            crossorigin="anonymous"></script>
+    @endif
 </body>
 
 </html>
