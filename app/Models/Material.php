a97eae61c430746cd7d068ec45c82f574547bbb2
McNamara <holgerehrmann@gmail.com>
2025-06-02 16:48:27 +0200

    Added HasFactory for later use in unit tests

--- app/Models/Material.php
+++ app/Models/Material.php
@@ -3,8 +3,10 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Material extends Model
 {
-    //
+    // HasFactory enables the use of model factories for testing
+    use HasFactory;
 }
