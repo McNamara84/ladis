a0158f64d35e8c1abf08802cbc9ba371321baa76
McNamara <holgerehrmann@gmail.com>
2025-06-03 07:05:48 +0200

    Added more columns and an example for text datatype

--- database/migrations/2025_06_02_174031_create_devices_table.php
+++ database/migrations/2025_06_02_174031_create_devices_table.php
@@ -13,6 +13,10 @@ return new class extends Migration
     {
         Schema::create('devices', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('institution_id')->constrained()->restrictOnDelete();
+            // text() creates a TEXT column for longer texts
+            $table->text('description')->nullable();
+            $table->string('name', 50)->unique();
             $table->timestamps();
         });
     }
