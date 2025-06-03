8f432b3007a6c29a03ff7847c5972d9b97556226
McNamara <holgerehrmann@gmail.com>
2025-06-03 09:04:26 +0200

    Added casts for alls attirbutes of Device as precaution as we do not yet know how the data will come in

--- app/Models/Device.php
+++ app/Models/Device.php
@@ -7,10 +7,49 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use App\Models\Institution;
 
-
+/**
+ * Device Model
+ *
+ * - Belongs to an institution (n:1)
+ * - Has many lenses (n:m)
+ */
 class Device extends Model
 {
     use HasFactory;
+
+    /**
+     * Attribute casting for stronger type safety
+     */
+    protected $casts = [
+        'institution_id'  => 'integer',
+        'year'            => 'integer',
+        'build'           => 'integer',
+        'safety_class'    => 'integer',
+        'height'          => 'integer',
+        'width'           => 'integer',
+        'depth'           => 'integer',
+        'weight'          => 'decimal:2',
+        'fiber_length'    => 'decimal:2',
+        'cooling'         => 'integer',
+        'mounting'        => 'boolean',
+        'automation'      => 'boolean',
+        'beam_type'       => 'integer',
+        'emission_source' => 'integer',
+        'max_output'      => 'float',
+        'mean_output'     => 'float',
+        'max_wattage'     => 'float',
+        'wavelength'      => 'float',
+        'min_spot_size'   => 'float',
+        'max_spot_size'   => 'float',
+        'min_pf'          => 'float',
+        'max_pf'          => 'float',
+        'min_pw'          => 'float',
+        'max_pw'          => 'float',
+        'min_scan_width'  => 'float',
+        'max_scan_width'  => 'float',
+        'min_focal_length'=> 'float',
+        'max_focal_length'=> 'float',
+    ];
 
     /**
      * 1:n relationship to Institution (backwards)
