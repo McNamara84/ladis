f03b712b60ea984c1b81a4e8f4391b7459c917c8
McNamara <holgerehrmann@gmail.com>
2025-06-11 16:33:33 +0200

    Added test case for testing if hidden attributes are working

--- tests/Feature/UserTest.php
+++ tests/Feature/UserTest.php
@@ -9,12 +9,19 @@ use App\Models\User;
 
 class UserTest extends TestCase
 {
-    /**
-     * A basic feature test example.
-     */
     public function test_fillable_attributes(): void
     {
         $user = new User();
         $this->assertSame(['name', 'email', 'password'], $user->getFillable());
+    }
+
+    public function test_hidden_attributes_are_hidden_from_array(): void
+    {
+        $user = new User(['password' => 'secret', 'remember_token' => 'token']);
+
+        $this->assertSame(['password', 'remember_token'], $user->getHidden());
+        $array = $user->toArray();
+        $this->assertArrayNotHasKey('password', $array);
+        $this->assertArrayNotHasKey('remember_token', $array);
     }
 }
