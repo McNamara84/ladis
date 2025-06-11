3de3d1294c46cd2a1fa13fb7198c108e82303c80
mukkig <michael-g-0@web.de>
2025-06-11 18:32:23 +0200

    Update DamagePattern.php

--- app/Models/DamagePattern.php
+++ app/Models/DamagePattern.php
@@ -3,8 +3,21 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use App\Models\Condition;
 
 class DamagePattern extends Model
 {
-    //
+    use HasFactory;
+    protected $table = 'damage_patterns';
+
+    protected $fillable = [
+        'name',
+    ];
+    public function conditions(): HasMany
+{
+    return $this->hasMany(Condition::class);
+}
+
 }
