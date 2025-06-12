9585aeccc1cc901ede5246bf9369c04137f65199
ArturNeufeld <artur.neufeld@fh-potsdam.de>
2025-06-12 11:35:23 +0200

    Update create_configurations_table.php
    Changed data types and added digit count restrictions to mirror comments in data model table.

--- database/migrations/2025_06_07_185652_create_configurations_table.php
+++ database/migrations/2025_06_07_185652_create_configurations_table.php
@@ -14,17 +14,24 @@ return new class extends Migration
         Schema::create('configurations', function (Blueprint $table) {
             $table->id();
             $table->foreignId('lens_id')->constrained('lenses')
-                ->unsigned();
-            $table->float('focal_length')->unsigned()->comment('in mm');
+            $table->smallInteger('focal_length')->unsigned()
+                ->digitsBetween(1, 3)
+                ->comment('in mm');
             $table->float('output')->unsigned()->comment('in J/s (W)');
-            $table->float('pw')->unsigned()->comment('pulse width, in ns');
-                //pulse_width (pw)
-            $table->float('pf')->unsigned()->comment('pulse frequency, in kHz');
-                //pulse_frequency (pf)
-            $table->float('scan_frequency')->unsigned()->comment('in Hz');
-            $table->float('scan_width')->unsigned()->comment('in mm');
-            $table->float('spot_size')->unsigned()->comment('in mm²');
-            $table->float('fluence')->unsigned()->comment('in J/cm²');
+            $table->smallInteger('pw')->unsigned()
+                //pulse_width (pw)    
+                ->digitsBetween(1, 4)
+                ->comment('pulse width, in ns');
+            $table->smallInteger('pf')->unsigned()
+                //pulse_frequency (pf)    
+                ->digitsBetween(1, 3)
+                ->comment('pulse frequency, in kHz');
+            $table->tinyInteger('scan_frequency')->unsigned()
+                ->digitsBetween(1, 2)
+                ->comment('in Hz');
+            $table->decimal('scan_width', 3, 1)->unsigned()->comment('in mm');
+            $table->decimal('spot_size', 4, 1)->unsigned()->comment('in mm²');
+            $table->decimal('fluence', 5, 3)->unsigned()->comment('in J/cm²');
             $table->text('description')->nullable();
             $table->timestamps();
         });
