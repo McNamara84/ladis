196042dc1ad8a6af494d4cd54143e1da7edbd993
McNamara <holgerehrmann@gmail.com>
2025-06-12 17:59:15 +0200

    Step 3: Adding index() method

--- app/Http/Controllers/ChangelogController.php
+++ app/Http/Controllers/ChangelogController.php
@@ -6,5 +6,13 @@ use Illuminate\Http\Request;
 
 class ChangelogController extends Controller
 {
-    //
+    /**
+     * Display the changelog view.
+     *
+     * @return \Illuminate\View\View
+     */
+    public function index()
+    {
+        return view('changelog');
+    }
 }
