24154cd9a27317d61214268926bf9a2585053bd7
McNamara <holgerehrmann@gmail.com>
2025-06-12 20:09:22 +0200

    Step 4: Adding a route to the controller

--- routes/web.php
+++ routes/web.php
@@ -2,11 +2,13 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\ChangelogController;
 
 // Landing page for guests
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/changelog', [ChangelogController::class, 'index']);
 
 // Login page with route name login
 Route::get('/login', function () {
