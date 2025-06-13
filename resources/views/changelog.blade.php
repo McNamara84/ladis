6187c4daba00937adf5df202b49475b5212227e3
McNamara <holgerehrmann@gmail.com>
2025-06-13 02:37:26 +0200

    Step 5: Editing the view

--- resources/views/changelog.blade.php
+++ resources/views/changelog.blade.php
@@ -1,3 +1,18 @@
-<div>
-    <!-- Live as if you were to die tomorrow. Learn as if you were to live forever. - Mahatma Gandhi -->
+@extends('layouts.app')
+
+@section('title', 'Changelog - Laser-Projekt - FH Potsdam')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-10">
+            <div class="card">
+                <div class="card-body">
+                    <h1 class="card-title">Changelog</h1>
+                    <p class="card-text">Welcome to our changelog page!</p>
+                </div>
+            </div>
+        </div>
+    </div>
 </div>
+@endsection
