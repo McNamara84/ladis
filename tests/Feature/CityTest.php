b820c0c58fbc1d5b085a5731910acbd12572058c
McNamara <holgerehrmann@gmail.com>
2025-06-14 05:53:40 +0200

    Replaced default test case with a new test case if attributes are fillable

--- tests/Feature/CityTest.php
+++ tests/Feature/CityTest.php
@@ -5,16 +5,20 @@ namespace Tests\Feature;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
+use App\Models\City;
 
 class CityTest extends TestCase
 {
     /**
      * A basic feature test example.
      */
-    public function test_example(): void
+    public function test_fillable_attributes_are_defined(): void
     {
-        $response = $this->get('/');
-
-        $response->assertStatus(200);
+        $city = new City();
+        $this->assertSame([
+            'federal_state_id',
+            'name',
+            'postal_code',
+        ], $city->getFillable());
     }
 }
