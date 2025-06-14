2959946e7b3c6f81c9cecec026ac91e23fe2dcc6
McNamara <holgerehrmann@gmail.com>
2025-06-14 05:58:38 +0200

    Added test case for relation between City and FederalStae

--- tests/Feature/CityTest.php
+++ tests/Feature/CityTest.php
@@ -6,12 +6,11 @@ use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 use App\Models\City;
+use App\Models\FederalState;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class CityTest extends TestCase
 {
-    /**
-     * A basic feature test example.
-     */
     public function test_fillable_attributes_are_defined(): void
     {
         $city = new City();
@@ -20,5 +19,15 @@ class CityTest extends TestCase
             'name',
             'postal_code',
         ], $city->getFillable());
+    }
+
+    public function test_federal_state_relationship_is_belongsto(): void
+    {
+        $city = new City();
+        $relation = $city->federalState();
+
+        $this->assertInstanceOf(BelongsTo::class, $relation);
+        $this->assertInstanceOf(FederalState::class, $relation->getRelated());
+        $this->assertSame('federal_state_id', $relation->getForeignKeyName());
     }
 }
