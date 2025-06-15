7779c85992c9acca3efee80329c403d00ac67400
mukkig <michael-g-0@web.de>
2025-06-15 16:31:13 +0200

    Update advanced_search.blade.php

--- resources/views/advanced_search.blade.php
+++ resources/views/advanced_search.blade.php
@@ -1,3 +1,18 @@
-<div>
-    <!-- Well begun is half done. - Aristotle -->
+@extends('layouts.app')
+
+@section('title', $pageTitle)
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-10">
+            <div class="card">
+                <div class="card-body">
+                    <h1 class="card-title">Advanced Search</h1>
+                    <p class="card-text">Welcome to the Advanced Search!</p>
+                </div>
+            </div>
+        </div>
+    </div>
 </div>
+@endsection
