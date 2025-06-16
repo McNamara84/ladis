0931c1599f7f8ba8d495009cfd0e1ac3daedd457
mukkig <michael-g-0@web.de>
2025-06-16 20:11:05 +0200

    Update AdvancedSearchController.php
    lastUpdated' was removed since it might not be of interest to the page's readers.

--- app/Http/Controllers/AdvancedSearchController.php
+++ app/Http/Controllers/AdvancedSearchController.php
@@ -10,8 +10,7 @@ class AdvancedSearchController extends Controller
     public function index()
     {
         $pageTitle = 'Advanced Search - Laser-Projekt - FH Potsdam';
-        $lastUpdated = '2025-06-15';
 
-        return view('advanced_search', compact('pageTitle', 'lastUpdated'));
+        return view('advanced_search', compact('pageTitle'));
     }
 }
