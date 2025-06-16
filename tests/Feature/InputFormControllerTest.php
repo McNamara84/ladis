5bab0bfba255e225fe5c1ace68d5d732d0ea7390
Maria-Schendel <149330325+Maria-Schendel@users.noreply.github.com>
2025-06-16 14:50:38 +0200

    Update InputFormControllerTest.php
    Renamed 'input_form' to 'inputform_device' to match the name of the view.

--- tests/Feature/InputFormControllerTest.php
+++ tests/Feature/InputFormControllerTest.php
@@ -11,12 +11,12 @@ class InputFormControllerTest extends TestCase
     /**
      * A basic feature test example.
      */
-    public function test_input_form_view_is_displayed(): void
+    public function test_inputform_device_view_is_displayed(): void
     {
-        $response = $this->get('/input_form');
+        $response = $this->get('/inputform_device');
         
         $response->assertStatus(200);
-        $response->assertViewIs('input_form');
+        $response->assertViewIs('inputform_device');
         $response->assertSee('Input Form - Laser-Projekt - FH Potsdam');
     }
 }
