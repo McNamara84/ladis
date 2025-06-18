0b29f0d935233bf80adf243bffeb4c2a4fc18048
mukkig <michael-g-0@web.de>
2025-06-18 11:44:37 +0200

    Update advanced_search.blade.php
    minor adjustment for search icon

--- resources/views/advanced_search.blade.php
+++ resources/views/advanced_search.blade.php
@@ -21,7 +21,7 @@
                             </div>
                             <div class="d-flex justify-content-end">
                                 <button type="submit" class="btn btn-primary">
-                                    <i class="bi bi-plus-circle"></i> Search
+                                    <i class="bi bi-search"></i> Search
                                 </button>
                             </div>
                         </form>
