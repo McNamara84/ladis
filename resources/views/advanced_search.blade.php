d73b782268f1321fb5ad8cdabf9d9014e1681500
mukkig <michael-g-0@web.de>
2025-06-18 11:36:42 +0200

    Update advanced_search.blade.php
    I have added a rudimentary form with two search fields. The upper field allows users to search for device names, while the lower field is used to search for project names. Both fields are labeled with attributes that should be considered when processing the request.
    
    I also removed the use of the '$pageTitle' variable in the first <h1> tag because the value didn’t seem appropriate. Instead, I simply used “Advanced Search”.

--- resources/views/advanced_search.blade.php
+++ resources/views/advanced_search.blade.php
@@ -8,8 +8,23 @@
         <div class="col-md-10">
             <div class="card">
                 <div class="card-body">
-                    <h1 class="card-title text-primary">{{ $pageTitle }}</h1>
+                    <h1 class="card-title text-primary">Advanced Search</h1>
                     <p class="card-text">Welcome to the Advanced Search!</p>
+                        <form>
+                            <div class="mb-3"> 
+                                <label for="device_name" class="form-label"> device name:</label>
+                                <input type="text" id= "device_name" name= "device_name" class="form-control" placeholder="Enter your search term here.">
+                            </div>
+                            <div class="mb-3">
+                                <label for="project_name" class="form-label"> project name:</label>
+                                <input type="text" id= "project_name" name= "project_name" class="form-control" placeholder="Enter your search term here.">
+                            </div>
+                            <div class="d-flex justify-content-end">
+                                <button type="submit" class="btn btn-primary">
+                                    <i class="bi bi-plus-circle"></i> Search
+                                </button>
+                            </div>
+                        </form>
                 </div>
             </div>
         </div>
