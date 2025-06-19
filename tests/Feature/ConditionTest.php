02ea21f7927347091a8ebce48e2363631ab29726
McNamara <holgerehrmann@gmail.com>
2025-06-19 18:33:05 +0200

    Added test case to test the relationship between Condition and DamagePattern

--- tests/Feature/ConditionTest.php
+++ tests/Feature/ConditionTest.php
@@ -6,6 +6,8 @@ use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 use App\Models\Condition;
+use App\Models\DamagePattern;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class ConditionTest extends TestCase
 {
@@ -41,5 +43,15 @@ class ConditionTest extends TestCase
         $this->assertSame('50.68', $condition->lab_l);
         $this->assertSame('5.43', $condition->lab_a);
         $this->assertSame('3.00', $condition->lab_b);
+    }
+
+    public function test_damage_pattern_relationship_is_belongsto(): void
+    {
+        $condition = new Condition();
+        $relation = $condition->damagePattern();
+
+        $this->assertInstanceOf(BelongsTo::class, $relation);
+        $this->assertInstanceOf(DamagePattern::class, $relation->getRelated());
+        $this->assertSame('damage_pattern_id', $relation->getForeignKeyName());
     }
 }
