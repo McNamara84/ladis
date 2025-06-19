7cd097f30938c79392634df8f39be48917d94a56
mayawu280 <maya.wussow@icloud.com>
2025-06-19 13:35:50 +0200

    index() angelegt im Controller und er gibt den view für changelog zurück

--- app/Http/Controllers/DatenschutzController.php
+++ app/Http/Controllers/DatenschutzController.php
@@ -6,5 +6,10 @@ use Illuminate\Http\Request;
 
 class DatenschutzController extends Controller
 {
-    //
+    public function index()
+    {
+        return view('changelog');
+    }
 }
+// This controller handles the display of the Datenschutz (Data Protection) page.
+// It returns the 'changelog' view when the index method is called.
