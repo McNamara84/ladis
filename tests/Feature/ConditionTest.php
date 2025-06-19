977921b8e32c39ddb67d39f283c58ffc213863d6
McNamara <holgerehrmann@gmail.com>
2025-06-19 18:21:01 +0200

    Added test case to test if attribute casts in the condition model are applied

--- tests/Feature/ConditionTest.php
+++ tests/Feature/ConditionTest.php
@@ -26,4 +26,20 @@ class ConditionTest extends TestCase
             'adhesion',
         ], $condition->getFillable());
     }
+
+    public function test_casts_for_attributes_are_applied(): void
+    {
+        $condition = new Condition([
+            'wac' => '1.25',
+            'lab_l' => 50.678,
+            'lab_a' => '5.4321',
+            'lab_b' => 3,
+        ]);
+
+        $this->assertIsFloat($condition->wac);
+        $this->assertSame(1.25, $condition->wac);
+        $this->assertSame('50.68', $condition->lab_l);
+        $this->assertSame('5.43', $condition->lab_a);
+        $this->assertSame('3.00', $condition->lab_b);
+    }
 }
