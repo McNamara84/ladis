53775df4e0d957a7f9cf4c457871e94d6c3e0289
nicolaibach <nicolai.bach@mailbox.org>
2025-06-19 21:07:29 +0200

    fix: footer positioning with content overflow
    Ensure footer is properly pushed down when content exceeds viewport height
    while maintaining sticky footer behavior for shorter content.

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,5 +1,5 @@
 <!doctype html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}" class="h-100">
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}" class="min-vh-100">
 
 <head>
     <meta charset="utf-8">
@@ -22,7 +22,7 @@
     @endif
 </head>
 
-<body class="d-flex flex-column h-100">
+<body class="d-flex flex-column min-vh-100">
     <!-- SVG Symbols Sprite -->
     <svg xmlns="http://www.w3.org/2000/svg" class="d-none">
         <!-- Logo placeholder -->
