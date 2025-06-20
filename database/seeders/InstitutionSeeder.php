51948d91b26bf19e8852a27755938a4c2ec07ab6
mukkig <michael-g-0@web.de>
2025-06-20 11:07:25 +0200

    Update InstitutionSeeder.php
    The seeder file now contains a single run method that creates three institutions.
    I will add the missing values later for the name and contact_information attributes.
    These instances will be needed later to reference the corresponding institutions within my DeviceSeeder.

--- database/seeders/InstitutionSeeder.php
+++ database/seeders/InstitutionSeeder.php
@@ -1,17 +1,33 @@
 <?php
 
 namespace Database\Seeders;
-
+use App\Models\Institution;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
 class InstitutionSeeder extends Seeder
 {
     /**
-     * Run the database seeds.
+     * Run the institution seeds.
      */
     public function run(): void
     {
-        //
+        Institution::create([
+        'name' => '',
+        'type' => Institution::TYPE_MANUFACTURER,
+        'contact_information' => '',
+        ]);
+
+        Institution::create([
+        'name' => '',
+        'type' => Institution::TYPE_MANUFACTURER,
+        'contact_information' => '',
+        ]);
+
+        Institution::create([
+        'name' => '',
+        'type' => Institution::TYPE_MANUFACTURER,
+        'contact_information' => '',
+        ]);
     }
 }
