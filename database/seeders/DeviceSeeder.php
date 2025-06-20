6ed974548be45275f5f4d6551dc0bd2a41b9013f
mukkig <michael-g-0@web.de>
2025-06-20 20:18:18 +0200

    Update DeviceSeeder.php
    I have added a description to the useDataFromCsvAndCreateDevice() function.

--- database/seeders/DeviceSeeder.php
+++ database/seeders/DeviceSeeder.php
@@ -8,10 +8,17 @@ use Illuminate\Database\Seeder;
 
 class DeviceSeeder extends Seeder
 {
-    /**
-     * Run the device seeds.
-     */
-
+/**
+ * This function loads Data from a CSV by combining the header row as keys and the rows after as values. This
+ * data is then being used to create Devices. Also, values that are not included in the $data array are being added afterwards.
+ *
+ * @param string $path_to_csv This is the path to the CSV file
+ * @param string $separator   This is the delimiter that is being used in the CSV file
+ * @param int    $institution_id This is the ID of the institution which manufactured the device
+ * @param int    $last_edit_by This is the ID of the user that has edited the dataset last
+ *
+ * @return void                no value is being returned
+ */
     private function useDataFromCsvAndCreateDevice(string $path_to_csv, string $separator, int $institution_id, int $last_edit_by){
 
     $csv_file = fopen(base_path($path_to_csv), 'r');
