cab36488615eb37f80b9b43030304e57654f021b
nicolaibach <nicolai.bach@mailbox.org>
2025-06-21 08:55:39 +0200

    feat: update welcome page content and layout
    Finally! After all that, I finally managed to do what this
    PR was all about. Update the welcome page.
    
    - Apply a more modern layout that utilizes screen estate in an efficient way
    - Add basic information about the project and the features the app will provide

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -15,6 +15,84 @@
         </div>
     </div>
     <div class="container">
-        <!-- More content here -->
+        <!-- Project/dataset information section -->
+        <section class="row g-md-5 pb-md-5 mb-5 align-items-center">
+            <div class="col-lg-8 mb-5">
+                <h2 class="display-5 mb-3 fw-semibold lh-sm">Erfasste Daten</h2>
+                <p class="lead fw-normal">Das System dokumentiert umfassende Informationen zu Laserreinigungsverfahren für
+                    wissenschaftliche Forschung und Restaurierungspraxis.</p>
+            </div>
+            <div class="row gx-md-5">
+                <div class="col-lg-6 mb-5">
+                    <h3 class="fw-semibold">Statistiken</h3>
+                    <table class="table">
+                        <thead>
+                            <tr>
+                                <th>Schema</th>
+                                <th>Erfassten Datensätze</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td>Laserreinigung</td>
+                                <td>100</td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+                <div class="col-lg-6 mb-3">
+                    <h3 class="fw-semibold">Technische Parameter</h3>
+                    <p>
+                        Detaillierte Aufzeichnung der verwendeten Lasergeräte, Optiken und Einstellungen.
+                    </p>
+                    <h3 class="fw-semibold mt-5">Objekt- und Projektdokumentation</h3>
+                    <p>
+                        Erfassung von Artefakten und deren Materialien, Schadensmuster und Erhaltungszustand vor und nach
+                        der Laserreinigung. Projektdaten, Standortdaten und beteiligte Personen und Institutionen ergänzen
+                        die Dokumentation.
+                    </p>
+                </div>
+            </div>
+        </section>
+
+        <!-- Research features section -->
+        <section class="row g-md-5 pb-md-5 mb-5 align-items-center">
+            <div class="col-lg-8 mb-5">
+                <h2 class="display-5 mb-3 fw-semibold lh-sm">Funktionen</h2>
+                <p class="lead fw-normal">Das System bietet umfassende Recherche- und Analysefunktionen.</p>
+            </div>
+            <div class="row gx-md-5">
+                <div class="col-lg-8 mb-3">
+                    <h3 class="fw-semibold">Suche</h3>
+                    <p>
+                        Die einfache Suche ermöglicht die schnelle Recherche mit Schlagworten.
+                    </p>
+                    <p>
+                        <button class="btn btn-outline-primary"
+                            onclick="document.getElementById('nav-search-input').focus()">→ Jetzt suchen</button>
+                    </p>
+                    <h3 class="fw-semibold mt-5">Erweiterte Suche</h3>
+                    <p>
+                        Die erweiterte Suche bietet umfassende Filter- und Sortierfunktionen für detailliertere
+                        Recherchen.
+                    </p>
+                    <p>
+                        <a class="btn btn-outline-primary" href="{{ url('/search-advanced') }}">→ Recherche starten</a>
+                    </p>
+                </div>
+                <div class="col-lg-4 mb-5 text-secondary">
+                    <svg class="bi bi-fluid" aria-hidden="true">
+                        <use xlink:href="#bi-sliders"></use>
+                    </svg>
+                </div>
+            </div>
+        </section>
+
+        <!-- Research features section -->
+        <section class="row g-md-5 pb-md-5 mb-5 align-items-center">
+            <div class="col-lg-8 mb-5">
+                <h2 class="display-5 mb-3 fw-semibold lh-sm">… weitere Informationen folgen</h2>
+            </div>
+        </section>
     </div>
 @endsection
