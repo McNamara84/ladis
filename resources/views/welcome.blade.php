7fcd8c593738145ac725039362c5796259d83883
nicolaibach <nicolai.bach@mailbox.org>
2025-06-21 07:22:42 +0200

    feat: add basic project description to hero on front page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,9 +5,13 @@
     <div class="vh-section">
         <div class="container content hero">
             <h1 class="hero-title">{{ config('app.name') }}</h1>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque, quidem! Aut ad tempore architecto
-                dignissimos? Architecto sequi dolorem distinctio voluptatibus repellendus ullam voluptate labore
-                eum, amet magni fuga est qui.</p>
+            <p><span class="brand-name">{{ config('app.name') }}</span> ist ein Informationssystem zur Dokumentation von
+                Laserreinigungsverfahren bei der Restaurierung historischer Oberflächen. Das System wurde 2025 vom <a
+                    href="https://www.fh-potsdam.de/studium-weiterbildung/fachbereiche/fachbereich-stadt-bau-kultur">Fachbereich
+                    STADT | BAU | KULTUR</a> in Kooperation mit dem <a
+                    href="https://www.fh-potsdam.de/studium-weiterbildung/fachbereiche/fachbereich-informationswissenschaften">Fachbereich
+                    Informationswissenschaften</a> der <a href="https://www.fh-potsdam.de">Fachhochschule Potsdam (FHP) –
+                    University of Applied Sciences</a> entwickelt.</p>
         </div>
     </div>
     <div class="container">
