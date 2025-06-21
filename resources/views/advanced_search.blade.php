c6aecb5d7e0c50418de8dd2e469e99de0b4dc099
Maria-Schendel <149330325+Maria-Schendel@users.noreply.github.com>
2025-06-21 20:46:21 +0200

    Feature: Deleted projectdate and added federal state name
    I deleted the project dates because there could be some conflicts due to the format of the date.

--- resources/views/advanced_search.blade.php
+++ resources/views/advanced_search.blade.php
@@ -13,25 +13,21 @@
                         <form>
 
                             <div class="mb-3">
+                                <label for="federal_state_name" class="form-label"> Bundesland</label>
+                                <input type="text" id="federal_state_name" name="federal_state_name" class="form-control"
+                                    placeholder="Geben Sie das Bundesland an">
+                            </div>
+
+                            <div class="mb-3">
                                 <label for="project_name" class="form-label"> Projektname</label>
                                 <input type="text" id="project_name" name="project_name" class="form-control"
                                     placeholder="Geben Sie den Namen des Projekts an">
                             </div>
 
                             <div class="mb-3">
-                                <label for="project_started_at" class="form-label">Projektstart</label>
-                                <input type="date" class="form-control" id="project_started_at" name="project_started_at">
-                            </div>
-
-                            <div class="mb-3">
-                                <label for="project_ended_at" class="form-label">Projektende</label>
-                                <input type="date" class="form-control" id="project_ended_at" name="project_ended_at">
-                            </div>
-
-                            <div class="mb-3">
                                 <label for="person_name" class="form-label">Projektleitung
                                     <select class="form-control" id="person_name" name="person_name" size="3">
-                                        <option disabled selected value="">Bitte wählen Sie die Projektleitung aus
+                                        <option disabled selected value="">Wählen Sie die Projektleitung aus
                                         </option>
                                         <option>Name1</option>
                                         <option>Name2</option>
@@ -40,28 +36,13 @@
                                         <option>Name5</option>
                                         <option>Name5</option>
                                     </select>
-                                    </div>
+                            </div>
 
                             <div class="mb-3">
-                                <label for="device_name" class="form-label">Gerätename
-                                    <select class="form-control" id="device_name" name="device_name">
-                                        <option disabled selected value="">Bitte wählen Sie den Namen des Lasergeräts aus
-                                        </option>
-                                        <option>CL20</option>
-                                        <option>CL50</option>
-                                        <option>Infinito Laser p/n I054C1</option>
-                                        <option>Thunder Compact</option>
-                                        <option>Soliton LT300</option>
-                                        <option>Smart 300</option>
-                                        <option>Sonstiges</option>
-                                    </select>
-                                    </div>
-
-
-                             <div class="mb-3">
                                 <label for="institution_name" class="form-label">Institution</label>
                                 <select class="form-control" id="institution_name" name="institution_name" size="3">
-                                    <option disabled selected value="">Bitte wählen Sie den Namen der Institution aus</option>
+                                    <option disabled selected value="">Wählen Sie den Namen der Institution aus
+                                    </option>
                                     <option>FH Potsdam</option>
                                     <option>Institution2</option>
                                     <option>Institution3</option>
@@ -72,9 +53,24 @@
                             </div>
 
                             <div class="mb-3">
+                                <label for="device_name" class="form-label">Gerätename
+                                    <select class="form-control" id="device_name" name="device_name">
+                                        <option disabled selected value="">Wählen Sie den Namen des Lasergeräts aus
+                                        </option>
+                                        <option>CL20</option>
+                                        <option>CL50</option>
+                                        <option>Infinito Laser p/n I054C1</option>
+                                        <option>Thunder Compact</option>
+                                        <option>Soliton LT300</option>
+                                        <option>Smart 300</option>
+                                        <option>Sonstiges</option>
+                                    </select>
+                            </div>
+
+                            <div class="mb-3">
                                 <label for="material_name" class="form-label">Material</label>
                                 <select class="form-control" id="material_name" name="material_name" size="3">
-                                    <option disabled selected value="">Bitte wählen Sie das Material aus</option>
+                                    <option disabled selected value="">Wählen Sie das Material aus</option>
                                     <option>Holz</option>
                                     <option>Stein</option>
                                     <option>Material2</option>
