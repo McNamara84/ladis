6ef669b5d3ce5f9fe2fe0fd0bda8213572b36e78
mukkig <michael-g-0@web.de>
2025-06-22 15:58:35 +0200

    Update DeviceSeeder.php
    formatted the file

--- database/seeders/DeviceSeeder.php
+++ database/seeders/DeviceSeeder.php
@@ -8,27 +8,28 @@ use Illuminate\Database\Seeder;
 
 class DeviceSeeder extends Seeder
 {
-/**
- * This function loads Data from a CSV by combining the header row as keys and the rows after as values. This
- * data is then being used to create Devices. Also, values that are not included in the $data array are being added afterwards (such as 
- * foreign keys)
- *
- * @param string $path_to_csv This is the path to the CSV file
- * @param string $delimiter   This is the delimiter that is being used in the CSV file
- * @param int    $institution_id This is the ID of the institution which manufactured the device
- * @param int    $last_edit_by This is the ID of the user that has edited the dataset last
- *
- * @return void                no value is being returned
- */
-    private function useDataFromCsvAndCreateDevice(string $path_to_csv, string $delimiter, int $institution_id, int $last_edit_by){
+    /**
+     * This function loads Data from a CSV by combining the header row as keys and the rows after as values. This
+     * data is then being used to create Devices. Also, values that are not included in the $data array are being added afterwards (such as 
+     * foreign keys)
+     *
+     * @param string $path_to_csv This is the path to the CSV file
+     * @param string $delimiter   This is the delimiter that is being used in the CSV file
+     * @param int    $institution_id This is the ID of the institution which manufactured the device
+     * @param int    $last_edit_by This is the ID of the user that has edited the dataset last
+     *
+     * @return void                no value is being returned
+     */
+    private function useDataFromCsvAndCreateDevice(string $path_to_csv, string $delimiter, int $institution_id, int $last_edit_by)
+    {
 
-    $csv_file = fopen(base_path($path_to_csv), 'r');
-        $header = null;
-        while (($row = fgetcsv($csv_file, null,$delimiter)) !== false) {
-            if ($header == null) {
-                $header = $row;
+        $csv_file = fopen(base_path($path_to_csv), 'r');
+        $header_row = null;
+        while (($row = fgetcsv($csv_file, null, $delimiter)) !== false) {
+            if ($header_row == null) {
+                $header_row = $row;
             } else {
-                $data = array_combine($header, $row);
+                $data = array_combine($header_row, $row);
                 Device::create(
                     array_merge(
                         $data,
@@ -43,10 +44,10 @@ class DeviceSeeder extends Seeder
         fclose($csv_file);
     }
 
-       public function run(): void
-    { 
-        $this->useDataFromCsvAndCreateDevice('database\data\devices\CL20_Backpack.csv',';',2,1);
-        $this->useDataFromCsvAndCreateDevice('database\data\devices\THUNDER_COMPACT.csv',';',1,1);
-        $this->useDataFromCsvAndCreateDevice('database\data\devices\Infinito Laser_p_n _I054C1.csv',';',1,1);
-}
+    public function run(): void
+    {
+        $this->useDataFromCsvAndCreateDevice('database\data\devices\CL20_Backpack.csv', ';', 2, 1);
+        $this->useDataFromCsvAndCreateDevice('database\data\devices\THUNDER_COMPACT.csv', ';', 1, 1);
+        $this->useDataFromCsvAndCreateDevice('database\data\devices\Infinito Laser_p_n _I054C1.csv', ';', 1, 1);
+    }
 }
