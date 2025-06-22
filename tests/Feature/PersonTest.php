ba7699d1e8574ec6f44825d65bb1bf6ff07b969f
nicolaibach <nicolai.bach@mailbox.org>
2025-06-22 12:52:31 +0200

    test(Person): add test for Project relation
    had to stop here because the relation between `Project` and `Venue` is
    not implemented in the Models but it's `NOT NULL` in the Schema.

--- tests/Feature/PersonTest.php
+++ tests/Feature/PersonTest.php
@@ -6,7 +6,7 @@ use App\Models\Institution;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
 use App\Models\Person;
-
+use App\Models\Project;
 class PersonTest extends TestCase
 {
     use RefreshDatabase;
@@ -14,16 +14,46 @@ class PersonTest extends TestCase
     private function createInstitution(): Institution
     {
         return Institution::create([
-            'name' => 'Insta',
+            'name' => fake()->company(),
             'type' => Institution::TYPE_CLIENT,
             'contact_information' => 'Send pigeon',
         ]);
     }
 
+    private function createProject($name, $person): Project
+    {
+        $project = new Project([
+            'name' => $name,
+            'description' => 'Description of the project',
+            'url' => 'https://foo.none',
+            'started_at' => fake()->date(),
+            'ended_at' => fake()->date(),
+        ]);
+
+        // Assign the person relationship in a separate step
+        $project->person()->associate($person);
+        $project->save();
+
+        return $project;
+    }
+
+    private function createProjects($person): array
+    {
+        $other_person = $this->createPerson($this->createInstitution());
+
+        return [
+            // Associated with the person
+            $this->createProject('Project Number 7', $person),
+            $this->createProject('Project E', $person),
+            // Not associated with the person
+            $this->createProject('Project 2501', $other_person),
+        ];
+    }
+
     private function createPerson($institution): Person
     {
         return Person::create([
-            'name' => 'Alice',
+            'name' => fake()->name(),
             'institution_id' => $institution->id,
         ]);
     }
@@ -45,5 +75,15 @@ class PersonTest extends TestCase
         $person = $this->createPerson($institution);
 
         $this->assertTrue($person->institution->is($institution));
+    }
+
+    public function test_person_projects_relationship(): void
+    {
+        $person = $this->createPerson($this->createInstitution());
+        $projects = $this->createProjects($person);
+
+        $this->assertTrue($person->projects->contains($projects[0]));
+        $this->assertTrue($person->projects->contains($projects[1]));
+        $this->assertFalse($person->projects->contains($projects[2]));
     }
 }
