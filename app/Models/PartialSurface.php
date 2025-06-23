d6bfd8382e78215318650470887afcdb1aee13f0
ArturNeufeld <artur.neufeld@fh-potsdam.de>
2025-06-23 11:49:36 +0200

    Fix: "PartialSurface" model relations
    Added "HasOne" relation type, changed relation types and added correct relationship commentary.

--- app/Models/PartialSurface.php
+++ app/Models/PartialSurface.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasOne;
 use App\Models\SampleSurface;
 use App\Models\Process;
 use App\Models\Condition;
@@ -13,14 +14,13 @@ use App\Models\Material;
 /**
  * PartialSurface Model:
  * 
- * - belongs to a sample_surface (n:1)
- * - belongs to a process (1:1)
- * - has a (pre-)condition (1:1)
- * - has a (result) condition (1:1)
- * - has a (foundation) material (n:1)
- * - has a (coating) material (n:1)
+ * - belongs to SampleSurface (n:1)
+ * - has one Process (1:1)
+ * - has one (pre-)Condition (1:1)
+ * - has one (result) Condition (1:1)
+ * - belongs to (foundation) Material (n:1)
+ * - belongs to (coating) Material (n:1)
  */
-
 class PartialSurface extends Model
 {
     use HasFactory;
@@ -48,28 +48,27 @@ class PartialSurface extends Model
     /**
      * 1:1 relationship to Process
      */
-    public function process(): BelongsTo
+    public function process(): HasOne
     {
-        return $this->belongsTo(Process::class);
+        return $this->hasOne(Process::class);
     }
 
     /**
      * 1:1 relationship to Condition as the pre-condition
      */
-    public function condition(): BelongsTo
+    public function condition(): HasOne
     {
-      return $this->belongsTo(Condition::class, 'condition_id');
+      return $this->hasOne(Condition::class, 'condition_id');
     }
     
      /**
      * 1:1 relationship to Condition as the result
      */
-    public function result(): BelongsTo
+    public function result(): HasOne
     {
-       return $this->belongsTo(Condition::class, 'result_id');
+       return $this->hasOne(Condition::class, 'result_id');
     }
     
-
     /**
      * n:1 relationship to Material as the foundation material (backwards)
      */
