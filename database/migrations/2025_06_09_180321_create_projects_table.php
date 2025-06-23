a9b8cb76d30695dbd14a52747aef863071f5570b
ArturNeufeld <artur.neufeld@fh-potsdam.de>
2025-06-23 23:45:54 +0200

    Update 2025_06_09_180321_create_projects_table.php
    Edited restrictions of "foreignId" table.

--- database/migrations/2025_06_09_180321_create_projects_table.php
+++ database/migrations/2025_06_09_180321_create_projects_table.php
@@ -22,15 +22,17 @@ return new class extends Migration {
             // Default format for the datatype date is "YYYY-MM-DD" 
             $table->date(column: 'started_at');
             $table->date(column: 'ended_at');
-
-
             $table->timestamps();
 
             // Foreign key references to the table person 
-            $table->foreignId('person_id')->constrained('persons');
+            $table->foreignId('person_id')->constrained('persons')
+                ->restrictOnDelete()
+                ->restrictOnUpdate();
 
             // Foreign key references to the table venue
-            $table->foreignId('venue_id')->constrained('venues');
+            $table->foreignId('venue_id')->constrained('venues')
+                ->restrictOnDelete()
+                ->restrictOnUpdate();
         });
     }
 
