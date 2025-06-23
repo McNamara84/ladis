0fe2d2376269be69c37fa195ae2fbf77c120dd9a
ArturNeufeld <artur.neufeld@fh-potsdam.de>
2025-06-23 11:51:05 +0200

    Fix: "Person" model relations
    Added "HasMany" relation type, added "use" lines for referenced models, added correct commentary for relationship functions.

--- app/Models/Person.php
+++ app/Models/Person.php
@@ -5,7 +5,16 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use App\Models\Institution;
+use App\Models\Project;
 
+/**
+ * Person Model:
+ *
+ * - belongs to Institution (n:1)
+ * - has many Projects (1:n)
+ */
 class Person extends Model
 {
     use HasFactory;
@@ -17,15 +26,22 @@ class Person extends Model
         'name' => 'string', 
     ];
 
-
-    //1:1 relation: A Person belongs to an Institution
-
-    public function institutions(): BelongsTo
+/**
+ * n:1 relationship to Institution (backwards)
+ */
+    public function institution(): BelongsTo
 {
     return $this->belongsTo(Institution::class);
 
 }
 
+/**
+ * 1:n relationship to Project
+ */
+    public function projects(): HasMany
+{
+    return $this->hasMany(Project::class);
 
+}
 
 }
