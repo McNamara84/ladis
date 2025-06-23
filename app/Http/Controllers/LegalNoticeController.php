8f6a96e9e886e8447b6f1cfb94928321ca7cf185
mayawu280 <maya.wussow@icloud.com>
2025-06-23 17:47:12 +0200

    index() method in Controller

--- app/Http/Controllers/LegalNoticeController.php
+++ app/Http/Controllers/LegalNoticeController.php
@@ -6,5 +6,11 @@ use Illuminate\Http\Request;
 
 class LegalNoticeController extends Controller
 {
-    //
+     public function index()
+    {
+        $pageTitle = 'Datenschutzerklärung';
+        $lastUpdated = '2025-06-21T00:00:00Z';
+
+        return view('privacy-policy', ['pageTitle' => $pageTitle, 'lastUpdated' => $lastUpdated]);
+    }
 }
