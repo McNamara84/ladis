60c935256a719182f8e3ff36c5f82ab1dd228180
mukkig <michael-g-0@web.de>
2025-06-24 23:24:19 +0200

    Update UserSeeder.php
    Adjusted the seeder logic.
    
    The run() method now checks if a user with ID 1 exists in the database.
    If the user exists, nothing happens. Otherwise, a BOT user is created with the ID set to 1.

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -11,12 +11,25 @@ class UserSeeder extends Seeder
     /**
      * Run the user seeds.
      */
+
+    /**
+     * This method creates a BOT user if it does not already exist in the database.
+     * The BOT user should always be the first user, which is why the 'id' is set to 1.
+     */
+
     public function run(): void
     {
-        User::factory()->create([
-            'id' => 1,
-            'name' => 'BOT',
-            'email' => 'bot@example.com',
-        ]);
+        $user = User::find(1);
+        if (!$user) {
+            User::factory()->create([
+                'id' => 1,
+                'name' => 'BOT',
+                'email' => 'bot@example.com',
+            ]);
+
+        }
+
     }
+
 }
+
