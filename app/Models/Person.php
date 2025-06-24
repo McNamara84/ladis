9a3b6aee5ee0f25c712b10841900f1572721042f
ArturNeufeld <artur.neufeld@fh-potsdam.de>
2025-06-24 21:26:43 +0200

    Update Person.php
    Added provided bug fix for "persons" table name (Laravel expecting "people" by default).

--- app/Models/Person.php
+++ app/Models/Person.php
@@ -19,6 +19,16 @@ class Person extends Model
 {
     use HasFactory;
 
+    /**
+     * The table associated with the model.
+     *
+     * Must be explicitly defined to prevent Eloquent from using 'people'
+     * instead of the actual 'persons' table name.
+     *
+     * @var string
+     */
+    protected $table = 'persons';
+
     protected $guarded = ['id'];
     
     protected $casts = [
