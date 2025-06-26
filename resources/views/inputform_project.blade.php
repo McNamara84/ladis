25d1c578c2d56afc7d743fd360688d37c8298bec
Maria-Schendel <149330325+Maria-Schendel@users.noreply.github.com>
2025-06-26 19:27:01 +0200

    Added the other input fields for project

--- resources/views/inputform_project.blade.php
+++ resources/views/inputform_project.blade.php
@@ -14,22 +14,61 @@
                 <div class="card-body">
                     <form>
                         <div class="mb-3">
-                            <label for="project_name" class="form-label">Projektname <span
+                            <label for="project_name" class="form-label">Name <span
                                     class="text-danger">*</span></label>
                             <input type="text" class="form-control" id="project_name" name="project_name" required
                                 placeholder="Geben Sie den Namen des Projektes an">
                             <div class="form-text">
-                                Bitte geben Sie eine eindeutige Bezeichnung für das Projekt an
+                                Bitte geben Sie eine eindeutige Bezeichnung für das Projekt an.
+                            </div>
+                        </div>
+
+                        <div class="mb-3">
+                            <label for="project_description" class="form-label">Beschreibung <span
+                                    class="text-danger">*</span></label>
+                            <textarea class="form-control" id="project_description" name="project_description" required
+                                rows="3" required placeholder="Geben Sie die Beschreibung des Projektes an"></textarea>
+                            <div class="form-text">
+                                Bitte geben Sie eine Beschreibung für das Projekt an.
+                            </div>
+                        </div>
+
+                        <div class="mb-3">
+                            <label for="project_url" class="form-label">URL <span class="text-danger">*</span></label>
+                            <input type="text" class="form-control" id="project_url" name="project_url" required
+                                placeholder="Geben Sie die URL des Projektes an">
+                            <div class="form-text">
+                                Bitte geben Sie eine eindeutige URL des Projektes an.
+                            </div>
+                        </div>
+
+                        <div class="mb-3">
+                            <label for="project_started_at" class="form-label">Beginn <span
+                                    class="text-danger">*</span></label>
+                            <input type="date" class="form-control" id="project_started_at" name="project_started_at"
+                                required placeholder="Geben Sie das Startdatum des Projektes an">
+                            <div class="form-text">
+                                Bitte geben Sie das Startdatum des Projektes an.
+                            </div>
+                        </div>
+
+                        <div class="mb-3">
+                            <label for="project_ended_at" class="form-label">Ende <span
+                                    class="text-danger">*</span></label>
+                            <input type="date" class="form-control" id="project_ended_at" name="project_ended_at"
+                                required placeholder="Geben Sie das Enddatum des Projektes an">
+                            <div class="form-text">
+                                Bitte geben Sie das Enddatum des Projektes an.
                             </div>
                         </div>
 
 
 
 
-
-
-
-
-
                     </form>
                 </div>
+
+            </div>
+        </div>
+    </div>
+</div>
