7539c6762e017a628bc315aa2b8e8d31056a5f09
McNamara <holgerehrmann@gmail.com>
2025-06-26 11:40:08 +0200

    Add user management view layout and table
    Replaces placeholder content with a structured user management page. Adds a table for displaying user data and a button to create a new account, using Bootstrap classes for layout and styling.

--- resources/views/user_management.blade.php
+++ resources/views/user_management.blade.php
@@ -1,3 +1,36 @@
-<div>
-    <!-- Very little is needed to make a happy life. - Marcus Aurelius -->
-</div>
+@extends('layouts.app')
+
+@section('title', 'Benutzerverwaltung')
+
+@section('content')
+    <div class="container">
+        <div class="row justify-content-between align-items-center mb-3">
+            <div class="col">
+                <h1 class="h3 mb-0">Benutzerverwaltung</h1>
+            </div>
+        </div>
+        <div class="table-responsive">
+            <table class="table table-striped">
+                <thead>
+                    <tr>
+                        <th>ID</th>
+                        <th>Name</th>
+                        <th>E-Mail</th>
+                        <th>Erstellt am</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>PH 1</td>
+                        <td>PH Max Mustermann</td>
+                        <td>PH example@test.de</td>
+                        <td>PH 02.03.2025 11:22</td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+        <div class="col-auto">
+            <a href="#" class="btn btn-primary">Neuen Account erstellen</a>
+        </div>
+    </div>
+@endsection
