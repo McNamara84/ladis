300f0add56387196dadb60f0cd40b0c8b459eede
mukkig <michael-g-0@web.de>
2025-06-30 13:55:49 +0200

    Changed name of file to make it specific
    Changed name of file to make it specific

--- tests/Feature/MaterialInputFormRouteTest.php
+++ tests/Feature/MaterialInputFormRouteTest.php
@@ -6,14 +6,14 @@ use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 
-class MaterialFormRouteTest extends TestCase
+class MaterialInputFormRouteTest extends TestCase
 {
     /**
      * A basic feature test example.
      */
     public function test_inputform_material_view_is_displayed(): void
     {
-        $response = $this->get('xxx/');
+        $response = $this->get('/inputform_material');
 
         $response->assertStatus(200);
         $response->assertViewIs('inputform_material');
