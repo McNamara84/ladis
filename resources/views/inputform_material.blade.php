02a25c9d5d8e2b199c52c6e6d611e116ca07d831
Maria-Schendel <149330325+Maria-Schendel@users.noreply.github.com>
2025-06-30 13:48:35 +0200

    Added "material name" as a input field for the view

--- resources/views/inputform_material.blade.php
+++ resources/views/inputform_material.blade.php
@@ -1,3 +1,33 @@
-<div>
-    <!-- Knowing is not enough; we must apply. Being willing is not enough; we must do. - Leonardo da Vinci -->
-</div>
+@extends('layouts.app')
+
+@section('title', 'Eingabeformular Device')
+
+@section('content')
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="card-header">
+                        <h4 class="mb-0">Neues Material hinzufügen</h4>
+                    </div>
+                    <div class="card-body">
+
+                        <div class="mb-3">
+                            <label for="material_name" class="form-label">Material <span
+                                    class="text-danger">*</span></label>
+                            <input type="text" class="form-control" id="material_name" name="material_name" required
+                                placeholder="Geben Sie den Namen für das Material an">
+                            <div class="form-text">
+                                Bitte geben Sie einen Namen für das Material an
+                            </div>
+                        </div>
+
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+@endsection
