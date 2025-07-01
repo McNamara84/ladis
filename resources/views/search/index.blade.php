aae35f7ef8ff5d9f37254542d49d1274117367f8
McNamara <holgerehrmann@gmail.com>
2025-07-01 18:32:10 +0200

    Add search results layout to search index view
    Replaces placeholder with a structured layout for search results, including sections for filters and device results. Adds Blade template structure and placeholders for future implementation.

--- resources/views/search/index.blade.php
+++ resources/views/search/index.blade.php
@@ -1,3 +1,23 @@
-<div>
-    <!-- The biggest battle is the war against ignorance. - Mustafa Kemal Atatürk -->
-</div>
+@extends('layouts.app')
+
+@section('title', $pageTitle)
+
+@section('content')
+    <div class="container py-4">
+        <div class="row">
+            <aside class="col-md-3 mb-4">
+                Placeholder for filters <!-- TODO: Implement filters as template -->
+            </aside>
+            <div class="col-md-9">
+                @if($query)
+                    <h1 class="h4">Suchergebnisse für "{{ $query }}"</h1>
+                @endif
+                @forelse($devices as $device)
+                    Placeholder for single results <!-- TODO: Implement device card as template -->
+                @empty
+                    <p>Keine Ergebnisse gefunden.</p>
+                @endforelse
+            </div>
+        </div>
+    </div>
+@endsection
