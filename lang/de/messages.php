004ea8e811cd6ba4895dbb4dc9bf079052696a08
ArturNeufeld <artur.neufeld@fh-potsdam.de>
2025-07-03 00:37:44 +0200

    Feature: example DE + EN localization files
    Created DE + EN "messages.php" localization files for translatable paragraphs. Filled EN "messages.php" and "en.json" files with translatable German lines from view "welcome.blade.php" as an example. Awaiting final builds for the remaining views before migrating lines into "messages.php" file.

---
+++ lang/de/messages.php
@@ -0,0 +1,40 @@
+<?php
+
+// lang/de/welcome.php
+// for: welcome.blade.php
+
+return [
+    'test' => 'Ich liebe Programmieren.',
+
+    /**
+     * --- views/layouts/app.blade.php ---
+     */
+
+    // Footer
+    '000' => 'Datenbank zu Reinigungslasern in der Restaurierung<br>Fachhochschule Potsdam - Studentisches Projekt<br>
+        <a href=":link00">Datenschutzerklärung</a>',
+    '001' => 'Vorname Nachname, Vorname Nachname, …<br>Vorname Nachname, Vorname Nachname, …',
+
+    /**
+     * --- views/welcome.blade.php ---
+     */
+
+    // LADIS
+    'a01' => 'ist ein Informationssystem zur Dokumentation von Laserreinigungsverfahren bei der Restaurierung historischer Oberflächen.',
+    'a02' => 'Das System wurde 2025 vom <a href=":link01">Fachbereich STADT | BAU | KULTUR</a> in Kooperation mit dem 
+        <a href=":link02">Fachbereich Informationswissenschaften</a> der <a href=":link03">Fachhochschule Potsdam (FHP)</a> entwickelt.',
+    
+    // Erfasste Daten
+    'b01' => 'Das System dokumentiert umfassende Informationen zu Laserreinigungsverfahren für wissenschaftliche Forschung und Restaurierungspraxis.',
+
+    // Technische Parameter
+    'c01' => 'Detaillierte Aufzeichnung der verwendeten Lasergeräte, Optiken und Einstellungen.',
+    'c02' => 'Erfassung von Artefakten und deren Materialien, Schadensmuster und Erhaltungszustand vor und nach der Laserreinigung. 
+        Projektdaten, Standortdaten und beteiligte Personen und Institutionen ergänzen die Dokumentation.',
+    
+    // Funktionen
+    'd01' => 'Das System bietet umfassende Recherche- und Analysefunktionen.',
+    'd02' => 'Die einfache Suche ermöglicht die schnelle Recherche mit Schlagworten.',
+    'd03' => 'Die erweiterte Suche bietet umfassende Filter- und Sortierfunktionen für detaillierte Recherchen.',
+];
+?>
