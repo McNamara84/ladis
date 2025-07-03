092aff4d3bf6ddc29e0cf409f13b22a3a8cca9c2
nicolaibach <nicolai.bach@mailbox.org>
2025-07-03 14:27:11 +0200

    refactor: update and fix form body layout

--- resources/views/inputform_material.blade.php
+++ resources/views/inputform_material.blade.php
@@ -21,31 +21,31 @@
                                 </div>
                             @endif
 
-                            <div class="row">
-                                <div class="col-md-3">
-                                    <label for="material_parent_id" class="form-label">Kategorie</label>
-                                    <select class="form-select" id="material_parent_id" name="material_parent_id">
-                                        <option value="">Bitte wählen Sie ein Material aus der Liste.</option>
-                                        @foreach($materials as $material)
-                                            <option value="{{ $material->id }}">{{ $material->name }}</option>
-                                        @endforeach
-                                    </select>
-                                </div>
+                            <div class="mb-5">
+                                <label for="material_parent_id" class="form-label">Kategorie</label>
+                                <select class="form-select" id="material_parent_id" name="material_parent_id">
+                                    <option value="">Bitte wählen Sie ein Material aus der Liste.</option>
+                                    @foreach($materials as $material)
+                                        <option value="{{ $material->id }}">{{ $material->name }}</option>
+                                    @endforeach
+                                </select>
+                            </div>
 
-                                <div class="mb-3">
-                                    <label for="material_name" class="form-label">Material <span
-                                            class="text-danger">*</span></label>
-                                    <input type="text" class="form-control" id="material_name" name="material_name"
-                                        value="{{ old('material_name') }}" required placeholder="Materialname" />
-                                    <div class="form-text">
-                                        Bitte wählen Sie ein Material aus der Liste.
-                                    </div>
+                            <div class="mb-5">
+                                <label for="material_name" class="form-label">Material <span
+                                        class="text-danger">*</span></label>
+                                <input type="text" class="form-control" id="material_name" name="material_name"
+                                    value="{{ old('material_name') }}" required placeholder="Materialname" />
+                                <div class="form-text">
+                                    Bitte wählen Sie ein Material aus der Liste.
                                 </div>
+                            </div>
 
-                                <div class="d-flex justify-content-end gap-2">
-                                    <button type="submit" class="btn btn-primary">Speichern</button>
-                                    <a href="{{ url()->previous() }}" class="btn btn-secondary">Abbrechen</a>
-                                </div>
+                            <div class="d-flex justify-content-end gap-2">
+                                <button type="submit" class="btn btn-primary">Speichern</button>
+                                <a href="{{ url()->previous() }}" class="btn btn-secondary">Abbrechen</a>
+                            </div>
+
                         </form>
                     </div>
                 </div>
