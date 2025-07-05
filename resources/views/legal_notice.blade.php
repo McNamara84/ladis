39fefc89501a98cd229f5fdde3b84011e0e8fe97
mayawu280 <maya.wussow@icloud.com>
2025-07-05 19:17:11 +0200

    Update legal notice formatting and content
    Changed heading levels and replaced <strong> tags with <span class="fw-bold"> for improved semantic structure and styling. Corrected a German term from singular to plural and removed the Datenschutzerklärung section.

--- resources/views/legal_notice.blade.php
+++ resources/views/legal_notice.blade.php
@@ -22,21 +22,21 @@
         <h2>Zuständige Institutionen</h2>
         <h3>Fachhochschule Potsdam</h3>
         <p>Die Fachhochschule Potsdam ist eine Körperschaft des Öffentlichen Rechts. Sie wird durch die amtierende Präsidentin / den amtierenden Präsidenten vertreten. Inhaltlich verantwortlich für die Inhalte dieses Webauftritts ist der Studiengang „Konservierung und Restaurierung“ der Fachhochschule Potsdam.</p>
-        <h3>Anschrift</h3>
+        <h4>Anschrift</h4>
         <address>
-            <strong>Fachhochschule Potsdam<br>
-            University of Applied Sciences</strong><br>
+            <span class="fw-bold">Fachhochschule Potsdam<br>
+            University of Applied Sciences</span><br>
             Kiepenheuerallee 5<br>
             14469 Potsdam<br>
             <br>
-            <strong>Telefon: </strong><a href="tel:+49 331 580-00">+49 331 580-00</a><br>
-            <strong>Fax: </strong>+49 331 580-1009<br>
-            <strong>E-mail: </strong><a href="mailto:info@fh-potsdam.de">info@fh-potsdam.de</a><br>
-            <strong>Webseite: </strong><a href="https://www.fh-potsdam.de">www.fh-potsdam.de</a><br>
+            <span class="fw-bold">Telefon:</span><a href="tel:+49 331 580-00">+49 331 580-00</a><br>
+            <span class="fw-bold">Fax: </span>+49 331 580-1009<br>
+            <span class="fw-bold">E-Mail: </span><a href="mailto:info@fh-potsdam.de">info@fh-potsdam.de</a><br>
+            <span class="fw-bold">Webseite: </span><a href="https://www.fh-potsdam.de">www.fh-potsdam.de</a><br>
         </address>
         <h2>Zuständige Aufsichtsbehörde</h2>
         <p>
-            <strong>Ministerium für Wissenschaft, Forschung und Kultur des Landes Brandenburg</strong><br>
+            <span class="fw-bold">Ministerium für Wissenschaft, Forschung und Kultur des Landes Brandenburg</span><br>
             Dortustr. 36<br>
             14467 Potsdam<br>
             <br>
@@ -64,7 +64,7 @@
             <li>Prof. Dr. Günther Neher</li>
             <li>Dipl. Phil. Wiss. Dok. Elena Semenova</li>
         </ul>
-        <p>und den Auftraggeber aus dem FB2 Architektur und Städtebau - Studiengang „Konservierung und Restaurierung“: </p>
+        <p>und den Auftraggebern aus dem FB2 Architektur und Städtebau - Studiengang „Konservierung und Restaurierung“: </p>
         <ul>
             <li>Lale Baudissin</li>
             <li>Dr. phil. Corinna Grimm-Remus </li>
@@ -78,7 +78,5 @@
         <p>Eine Haftung für die Richtigkeit, Vollständigkeit und Aktualität dieser Webseiten kann trotz sorgfältiger Prüfung nicht übernommen werden. Die Fachhochschule Potsdam übernimmt insbesondere keine Haftung für eventuelle Schäden oder Konsequenzen, die durch eine direkte oder indirekte Nutzung der angebotenen Inhalte entstehen. Es wird ausdrücklich darauf hingewiesen, dass für den Inhalt verlinkter Seiten ausschließlich deren Betreiber verantwortlich ist. Trotz sorgfältiger inhaltlicher Kontrolle übernehmen wir keine Haftung für die Inhalte externer Links. Für Schäden, die aus der Nutzung oder Nichtnutzung dieser Informationen entstehen, haftet ausschließlich der Betreiber der Seite, auf die verwiesen wurde. Sollten Sie Kenntnis von verlinkten Seiten mit rechtswidrigen Inhalten erlangen, bitten wir Sie, uns dies mitzuteilen.</p>
         <h2>Urheberrecht</h2>
         <p>Alle im Internetangebot des Studiengangs Kulturarbeit veröffentlichten Inhalte (Layout, Texte, Bilder, Grafiken usw.) unterliegen dem Urheberrecht. Jede vom Urheberrechtsgesetz nicht zugelassene Verwertung bedarf vorheriger ausdrücklicher Zustimmung. Dies gilt insbesondere für Vervielfältigung, Bearbeitung, Übersetzung, Einspeicherung, Verarbeitung bzw. Wiedergabe von Inhalten in Datenbanken oder anderen elektronischen Medien und Systemen. Fotokopien und Downloads von Webseiten für den privaten, wissenschaftlichen und nicht kommerziellen Gebrauch dürfen hergestellt werden. Wir erlauben ausdrücklich und begrüßen das Zitieren unserer Dokumente und Webseiten sowie das Setzen von Links auf unsere Website.</p>
-        <h2>Datenschutzerklärung</h2>
-        <p>Die Fachhochschule Potsdam unterliegt den Bestimmungen des Brandenburgischen Datenschutzgesetzes sowie den einschlägigen gesetzlichen Grundlagen mit Bestimmungen zum Schutz des Rechtes auf informationelle Selbstbestimmung. Zum Schutz Ihrer Rechte haben wir technische und organisatorische Maßnahmen getroffen, dass die Vorschriften über den Datenschutz sowohl innerhalb der Fachhochschule Potsdam als auch von externen Dienstleistern beachtet werden, wenn diese an diesem Angebot mitwirken. Ihre Daten dürfen nur in dem Umfange verarbeitet werden, wie spezielle Gesetze dies zulassen oder Ihre Einwilligung vorliegt. In Verbindung mit Ihrem Zugriff auf das Internetangebot der Fachhochschule Potsdam werden auf unseren Servern im Falle des erfolglosen Abrufens einer Datei vorübergehend Daten zur Feststellung von Betriebsstörungen gespeichert, die möglicherweise eine Identifizierung zulassen. Dies können sein: Name der abgerufenen Datei, Datum und Uhrzeit des Abrufs und die abrufende IP-Adresse.</p>
     </div>
 @endsection
