ee9cfaf29798d0d9772aa066cc79e7dcbe196612
nicolaibach <nicolai.bach@mailbox.org>
2025-07-05 06:02:45 +0200

    feat: add Nav/Item component class
    Add reusable navigation item component with route and text properties to reduce code duplication in navigation menus.

---
+++ app/View/Components/Nav/Item.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\View\Components\Nav;
+
+use Closure;
+use Illuminate\Contracts\View\View;
+use Illuminate\View\Component;
+
+/**
+ * Item component for the navigation bar
+ *
+ * @param string $route The route name
+ * @param string $text The display text
+ */
+class Item extends Component
+{
+    /**
+     * Create a new component instance.
+     *
+     * @param string $route The route name
+     * @param string $text The display text
+     */
+    public function __construct(
+        public string $route,
+        public string $text,
+    ) {
+    }
+
+    /**
+     * Get the view / contents that represent the component.
+     */
+    public function render(): View|Closure|string
+    {
+        return view('components.nav.item');
+    }
+}
