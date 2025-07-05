dc40e66914478c1aae8e06bdf123f20de09616fd
McNamara <holgerehrmann@gmail.com>
2025-07-05 17:13:02 +0200

    Enhance advanced search with institution filter
    The advanced search now supports filtering devices by institution name in addition to device name. This improves search flexibility for users needing to find devices by associated institution.

--- app/Http/Controllers/SearchController.php
+++ app/Http/Controllers/SearchController.php
@@ -10,15 +10,25 @@ class SearchController extends Controller
     public function search(Request $request)
     {
         $query = $request->input('q');
-        // Check if the request is comming from the advanced search
-        $advanced = $request->boolean('advanced');
+        $advanced = $request->boolean('advanced'); // Check if the request is comming from the advanced search
+        $institution = $request->input('institution_id');
 
         $devices = [];
-        if ($advanced && $query) {
-            // Search for devices by name
-            $devices = Device::where('name', 'like', "%{$query}%")->get();
-            // TODO: Implement advanced search for institutions
-            
+        if ($advanced && ($query || $institution)) {
+            $devices = Device::query();
+
+            if ($query) {
+                $devices->where('name', 'like', "%{$query}%");
+            }
+
+            if ($institution) {
+                $devices->orWhereHas('institution', function ($q) use ($institution) {
+                    $q->where('name', 'like', "%{$institution}%");
+                });
+            }
+
+            $devices = $devices->get();
+
         } elseif ($query) {
             $devices = Device::where('name', 'like', "%{$query}%")
             ->orWhereHas('institution', function ($q) use ($query) {
