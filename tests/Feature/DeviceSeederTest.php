4a43224f2ba4c8eabafb270a00a872eb70979497
mukkig <michael-g-0@web.de>
2025-07-06 22:06:36 +0200

    Update DeviceSeederTest.php
    Added private function to get the file count from the storage where all CSV files for device data are stored

--- tests/Feature/DeviceSeederTest.php
+++ tests/Feature/DeviceSeederTest.php
@@ -3,11 +3,19 @@
 namespace Tests\Feature;
 
 use Illuminate\Foundation\Testing\RefreshDatabase;
-use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Support\Facades\Storage;
 use Tests\TestCase;
+
 
 class DeviceSeederTest extends TestCase
 {
+    use RefreshDatabase;
+
+    private function get_file_count() {
+    $fileCount = count(Storage::disk('local')->files('devices'));
+
+    return $fileCount;
+    }
     /**
      * A basic feature test example.
      */
