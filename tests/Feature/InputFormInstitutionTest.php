e8b15da2e5c0ec384349a59822ef8ff3a523bcdb
mukkig <michael-g-0@web.de>
2025-07-06 15:35:08 +0200

    Update InputFormInstitutionTest.php
     Defines a test method to verify that a new record is created and the user is redirected correctly.

--- tests/Feature/InputFormInstitutionTest.php
+++ tests/Feature/InputFormInstitutionTest.php
@@ -2,9 +2,10 @@
 
 namespace Tests\Feature;
 
+use Database\Factories\InstitutionFactory;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
-
+use App\Models\Institution;
 class InputFormInstitutionTest extends TestCase
 {
     use RefreshDatabase;
@@ -19,4 +20,19 @@ class InputFormInstitutionTest extends TestCase
         $response->assertViewIs('inputform_institution');
 
 
+    }
+
+        public function test_store_creates_institution_and_redirects(): void
+    {
+        $record = Institution::factory()->make()->toArray();
+
+        $response = $this->withHeader('referer', '/inputform_institution')
+            ->post('/inputform_institution', $record);
+
+        $response->assertRedirect('/inputform_institution');
+        $this->assertDatabaseHas('institutions',[
+            'name' => $record['name'],
+            'type' => $record['type'],
+            'contact_information'=> $record['contact_information'],
+        ] );
     }
