89b98607c0dc82d5a49b6b8955ed25ac50ef46e5
ArturNeufeld <artur.neufeld@fh-potsdam.de>
2025-07-07 01:01:34 +0200

    Update advanced_search.blade.php
    Replaced German strings with English auto-translatable strings.

--- resources/views/advanced_search.blade.php
+++ resources/views/advanced_search.blade.php
@@ -8,14 +8,14 @@
             <div class="col-md-10">
                 <div class="card">
                     <div class="card-body">
-                        <h1 class="card-title text-primary">Erweiterte Suche</h1>
+                        <h1 class="card-title text-primary">{{ __("Advanced Search") }}</h1>
                         <form action="{{ route('search_results') }}" method="GET">
                             <input type="hidden" name="advanced" value="1">
 
                             <div class="mb-3">
-                                <label for="federal_state_id" class="form-label">Bundesland</label>
+                                <label for="federal_state_id" class="form-label">{{ __("Federate State" )}}</label>
                                 <select class="form-control" id="federal_state_id" name="federal_state_id">
-                                    <option disabled selected value="">Wählen Sie das Bundesland aus</option>
+                                    <option disabled selected value="">{{ __("Choose federate state …") }}</option>
                                     @foreach ($states as $state)
                                         <option value="{{ $state->id }}">{{ $state->name }}</option>
                                     @endforeach
@@ -23,21 +23,21 @@
                             </div>
 
                             <div class="mb-3">
-                                <label for="project_name" class="form-label"> Projektname</label>
+                                <label for="project_name" class="form-label">{{ __("Project Name") }}</label>
                                 <input type="text" id="project_name" name="project_name" class="form-control"
-                                    placeholder="Geben Sie den Namen des Projekts an">
+                                    placeholder="{{ __("Specify name of project …") }}">
                             </div>
 
                             <div class="mb-3">
-                                <label for="person_name" class="form-label"> Projektleitung</label>
+                                <label for="person_name" class="form-label">{{ __("Project Leader") }}</label>
                                 <input type="text" id="person_name" name="person_name" class="form-control"
-                                    placeholder="Geben Sie die Projektleitung an">
+                                    placeholder="{{ __("Specify name of project leader …") }}">
                             </div>
 
                             <div class="mb-3">
-                                <label for="institution_id" class="form-label">Institution</label>
+                                <label for="institution_id" class="form-label">{{ __("Institution") }}</label>
                                 <select class="form-control" id="institution_id" name="institution_id" size="3">
-                                    <option disabled selected value="">Wählen Sie den Namen der Institution aus</option>
+                                    <option disabled selected value="">{{ __("Choose institution …") }}</option>
                                     @foreach ($institutions as $institution)
                                         <option value="{{ $institution->name }}">{{ $institution->name }}</option>
                                     @endforeach
@@ -45,21 +45,18 @@
                             </div>
 
                             <div class="mb-3">
-                                <label for="device_name" class="form-label">Gerätename</label>
+                                <label for="device_name" class="form-label">{{ __("Device Model") }}</label>
                                 <input type="search" id="device_name" name="q" class="form-control"
-                                    placeholder="Geben Sie den Namen des Lasergeräts ein">
+                                    placeholder="{{ __("Specify name of device model …") }}">
                             </div>
 
                             <div class="mb-3">
-                                <label for="material_id" class="form-label">Material</label>
+                                <label for="material_id" class="form-label">{{ __("Material") }}</label>
                                 <select class="form-control" id="material_id" name="material_id" size="3">
-                                    <option disabled selected value="">Wählen Sie das Material aus</option>
-                                    <option>Holz</option>
-                                    <option>Stein</option>
-                                    <option>Material2</option>
-                                    <option>Material3</option>
-                                    <option>Material4</option>
-                                    <option>Sonstiges</option>
+                                    <option disabled selected value="">{{ __("Choose material …") }}</option>
+                                    <option>{{ __("Wood") }}</option>
+                                    <option>{{ __("Stone") }}</option>
+                                    <option>{{ __("Miscellaneous") }}</option>
                                 </select>
                             </div>
 
@@ -67,7 +64,7 @@
                                 <button type="submit" class="btn btn-outline-secondary">
                                     <svg class="bi" width="16" height="16" aria-hidden="true">
                                         <use xlink:href="#bi-search"></use>
-                                    </svg> Suchen
+                                    </svg> {{ __("Start Search") }}
                                 </button>
                             </div>
                         </form>
