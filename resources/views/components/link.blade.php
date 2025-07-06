1b49e823af963c8be46f045f0e848221a89605c2
nicolaibach <nicolai.bach@mailbox.org>
2025-07-06 08:18:22 +0200

    refactor: use icon component in link component instead of svg markup

--- resources/views/components/link.blade.php
+++ resources/views/components/link.blade.php
@@ -1,9 +1,7 @@
 <a href="{{ $href() }}" {{ $attributes->class(['disabled' => $disabled, 'active' => $isActive()]) }} @if($disabled)
 aria-disabled="true" @endif>
     @if($icon)
-        <svg class="bi me-2" width="16" height="16" aria-hidden="true">
-            <use xlink:href="#{{ $icon }}"></use>
-        </svg>
+        <x-icon :$icon class="me-2" />
     @endif
     {{ $text }}
 </a>
