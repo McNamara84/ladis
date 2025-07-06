bbdabfbcd184ddee98a960c68e27fcca953ff9a0
ArturNeufeld <artur.neufeld@fh-potsdam.de>
2025-07-07 01:06:28 +0200

    Update messages.php
    Added more referenceable German lines from various views.

--- lang/de/messages.php
+++ lang/de/messages.php
@@ -1,8 +1,5 @@
 <?php
 
-// lang/de/welcome.php
-// for: welcome.blade.php
-
 return [
     'test' => 'Ich liebe Programmieren.',
 
@@ -20,21 +17,43 @@ return [
      */
 
     // LADIS
-    'a01' => 'ist ein Informationssystem zur Dokumentation von Laserreinigungsverfahren bei der Restaurierung historischer Oberflächen.',
-    'a02' => 'Das System wurde 2025 vom <a href=":link01">Fachbereich STADT | BAU | KULTUR</a> in Kooperation mit dem 
+    'a00' => 'ist ein Informationssystem zur Dokumentation von Laserreinigungsverfahren bei der Restaurierung historischer Oberflächen.',
+    'a01' => 'Das System wurde 2025 vom <a href=":link01">Fachbereich STADT | BAU | KULTUR</a> in Kooperation mit dem 
         <a href=":link02">Fachbereich Informationswissenschaften</a> der <a href=":link03">Fachhochschule Potsdam (FHP)</a> entwickelt.',
     
     // Erfasste Daten
-    'b01' => 'Das System dokumentiert umfassende Informationen zu Laserreinigungsverfahren für wissenschaftliche Forschung und Restaurierungspraxis.',
+    'a02' => 'Das System dokumentiert umfassende Informationen zu Laserreinigungsverfahren für wissenschaftliche Forschung und Restaurierungspraxis.',
 
     // Technische Parameter
-    'c01' => 'Detaillierte Aufzeichnung der verwendeten Lasergeräte, Optiken und Einstellungen.',
-    'c02' => 'Erfassung von Artefakten und deren Materialien, Schadensmuster und Erhaltungszustand vor und nach der Laserreinigung. 
+    'a03' => 'Detaillierte Aufzeichnung der verwendeten Lasergeräte, Optiken und Einstellungen.',
+    'a04' => 'Erfassung von Artefakten und deren Materialien, Schadensmuster und Erhaltungszustand vor und nach der Laserreinigung. 
         Projektdaten, Standortdaten und beteiligte Personen und Institutionen ergänzen die Dokumentation.',
     
     // Funktionen
-    'd01' => 'Das System bietet umfassende Recherche- und Analysefunktionen.',
-    'd02' => 'Die einfache Suche ermöglicht die schnelle Recherche mit Schlagworten.',
-    'd03' => 'Die erweiterte Suche bietet umfassende Filter- und Sortierfunktionen für detaillierte Recherchen.',
+    'a05' => 'Das System bietet umfassende Recherche- und Analysefunktionen.',
+    'a06' => 'Die einfache Suche ermöglicht die schnelle Recherche mit Schlagworten.',
+    'a07' => 'Die erweiterte Suche bietet umfassende Filter- und Sortierfunktionen für detaillierte Recherchen.',
+
+    /**
+     * --- views/layouts/user_management.blade.php ---
+     */
+
+    'b00' => 'Soll der Account <strong>{{ :userName }}</strong> wirklich gelöscht werden?',
+
+    /**
+     * --- views/layouts/home.blade.php ---
+     */
+
+    // Welcome Header
+    'c00' => 'Willkommen zurück, {{ :userName }}!',
+    'c01' => 'Verwalten Sie Ihre Reinigungslaser-Datenbank und Restaurierungsprojekte.',
+    
+    // Quick Actions
+    'c02' => 'Starten Sie mit der Erfassung von Reinigungslasern',
+    
+    // Getting Started
+    'c03' => 'Willkommen im {{ :appName }}!',
+    'c04' => 'Beginnen Sie mit der Erfassung und Verwaltung von Reinigungslasern für Restaurierungsprojekte. 
+        Achtung: Das System befindet sich noch in der Entwicklung.'
+
 ];
-?>
