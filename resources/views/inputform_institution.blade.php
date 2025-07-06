7accfda8ef3ddec5ab528038ab48602653497c51
mukkig <michael-g-0@web.de>
2025-07-06 12:38:48 +0200

    Update inputform_institution.blade.php
    defined title and start of content section

--- resources/views/inputform_institution.blade.php
+++ resources/views/inputform_institution.blade.php
@@ -1,3 +1,13 @@
 <div>
     <!-- Act only according to that maxim whereby you can, at the same time, will that it should become a universal law. - Immanuel Kant -->
 </div>
+@extends('layouts.app')
+@section('title', 'Eingabeformular Institution')
+@section('content')
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-10">
+                <div class="card">
+                    <div class="card-header">
+                        <h4 class="mb-0">Neue Institution hinzufügen</h4>
+                    </div>
