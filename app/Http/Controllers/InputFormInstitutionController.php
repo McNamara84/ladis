9f19c9c62934d880a34d3477ab35d8616c801697
mukkig <michael-g-0@web.de>
2025-07-06 11:37:35 +0200

    Update InputFormInstitutionController.php
    Defined index method to pass the page title to the view.

--- app/Http/Controllers/InputFormInstitutionController.php
+++ app/Http/Controllers/InputFormInstitutionController.php
@@ -4,8 +4,18 @@ namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Http\RedirectResponse;
+use App\Models\Institution;
 
 class InputFormInstitutionController extends Controller
 {
-    //
+
+    public function index()
+    {
+        $pageTitle = 'Eingabeformular - Institution - LADIS - FH Potsdam';
+
+        return view('inputform_institution', compact('pageTitle'));
+    }
+
+
 }
