f41e43fd282bcf95f9c8890c8f9c3fa27ad02b55
mayawu280 <maya.wussow@icloud.com>
2025-07-07 10:17:58 +0200

    Add inventory number field to artifact input form
    Introduced a new required input for 'Inventarnummer' (inventory number = can be NULL) in the artifact creation form. Also updated the label and placeholder for the artifact name field for improved clarity.

--- resources/views/inputform_artifact.blade.php
+++ resources/views/inputform_artifact.blade.php
@@ -14,20 +14,26 @@
                     <div class="card-body">
                         <form method="POST" action="{{ route('inputform_artifact.store') }}">
                             <div class="mb-5">
-                                <label for="artifact_name" class="form-label">Objekt <span
+                                <label for="artifact_name" class="form-label">Objektname <span
                                         class="text-danger">*</span></label>
                                 <input type="text" class="form-control" id="artifact_name" name="artifact_name"
-                                    value="{{ old('artifact_name') }}" required placeholder="Objektname" />
-                                <div class="form-text">
-                                    Legen Sie den Name eines neuen Objekts an.
-                                </div>
+                                    value="{{ old('artifact_name') }}" required placeholder="Legen Sie den Name eines neuen Objekts an." />
                                 @error('artifact_name')
                                     <div class="invalid-feedback" role="alert">
                                         <strong>{{ $message }}</strong>
                                     </div>
                                 @enderror
                             </div>
-
+                            <div class="mb-5">
+                                <label for="artifact_inventory_number" class="form-label">Inventarnummer</label>
+                                <input type="text" class="form-control" id="artifact_inventory_number" name="artifact_inventory_number"
+                                    value="{{ old('artifact_inventory_number') }}" required placeholder="Geben Sie hier die Inventarnummer an." />
+                                @error('artifact_inventory_number')
+                                    <div class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </div>
+                                @enderror
+                            </div>
                             <div class="d-flex justify-content-end gap-2">
                                 <button type="submit" class="btn btn-primary">Speichern</button>
                                 <a href="{{ url()->previous() }}" class="btn btn-secondary">Abbrechen</a>
