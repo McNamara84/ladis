26da2b8ea1852720cd293866f9c4d7d7bd7e5fbb
ArturNeufeld <artur.neufeld@fh-potsdam.de>
2025-07-08 01:05:24 +0200

    Update inputform_project.blade.php

--- resources/views/inputform_project.blade.php
+++ resources/views/inputform_project.blade.php
@@ -1,6 +1,6 @@
 @extends('layouts.app')
 
-@section('title', 'Eingabeformular Projekt')
+@section('title', 'Input Form: Project')
 
 @section('content')
     <div class="container">
@@ -8,55 +8,52 @@
             <div class="col-md-8">
                 <div class="card">
                     <div class="card-header">
-                        <h4 class="mb-0">Neues Projekt anlegen</h4>
+                        <h4 class="mb-0">{{ __("Add New Project") }}</h4>
                     </div>
                     <div class="card-body">
                         <form>
                             <div class="mb-3">
-                                <label for="project_name" class="form-label">Name <span class="text-danger">*</span></label>
+                                <label for="project_name" class="form-label">{{ __("Project Title") }}<span class="text-danger"> *</span></label>
                                 <input type="text" class="form-control" id="project_name" name="project_name" required
-                                    placeholder="Geben Sie den Namen des Projektes an">
+                                    placeholder="{{ __("Specify project title …") }}">
                                 <div class="form-text">
-                                    Bitte geben Sie eine eindeutige Bezeichnung für das Projekt an.
+                                    {{ __("messages.f00") }}
                                 </div>
                             </div>
 
                             <div class="mb-3">
-                                <label for="project_description" class="form-label">Beschreibung <span
-                                        class="text-danger">*</span></label>
+                                <label for="project_description" class="form-label">{{ __("Description") }}<span class="text-danger"> *</span></label>
                                 <textarea class="form-control" id="project_description" name="project_description" required
-                                    rows="3" required placeholder="Geben Sie die Beschreibung des Projektes an"></textarea>
+                                    rows="3" required placeholder="{{ __("Add project description …") }}"></textarea>
                                 <div class="form-text">
-                                    Bitte geben Sie eine Beschreibung für das Projekt an.
+                                    {{ __("messages.f01") }}
                                 </div>
                             </div>
 
                             <div class="mb-3">
-                                <label for="project_url" class="form-label">URL <span class="text-danger">*</span></label>
+                                <label for="project_url" class="form-label">{{{ __("URL Adress") }}}<span class="text-danger"> *</span></label>
                                 <input type="text" class="form-control" id="project_url" name="project_url" required
-                                    placeholder="Geben Sie die URL des Projektes an">
+                                    placeholder="{{ __("Specify URL adress of project …") }}">
                                 <div class="form-text">
-                                    Bitte geben Sie eine eindeutige URL des Projektes an.
+                                    {{ __("messages.f02") }}
                                 </div>
                             </div>
 
                             <div class="mb-3">
-                                <label for="project_started_at" class="form-label">Beginn <span
-                                        class="text-danger">*</span></label>
+                                <label for="project_started_at" class="form-label">{{ __("Start") }}<span class="text-danger"> *</span></label>
                                 <input type="date" class="form-control" id="project_started_at" name="project_started_at"
-                                    required placeholder="Geben Sie das Startdatum des Projektes an">
+                                    required placeholder="{{ __("Specify start date …") }}">
                                 <div class="form-text">
-                                    Bitte geben Sie das Startdatum des Projektes an.
+                                    {{ __("messages.f03") }}
                                 </div>
                             </div>
 
                             <div class="mb-3">
-                                <label for="project_ended_at" class="form-label">Ende <span
-                                        class="text-danger">*</span></label>
+                                <label for="project_ended_at" class="form-label">{{ __("End") }}<span class="text-danger"> *</span></label>
                                 <input type="date" class="form-control" id="project_ended_at" name="project_ended_at"
-                                    required placeholder="Geben Sie das Enddatum des Projektes an">
+                                    required placeholder="{{ __("Specify end date …") }}">
                                 <div class="form-text">
-                                    Bitte geben Sie das Enddatum des Projektes an.
+                                    {{ __("messages.f04") }}
                                 </div>
                             </div>
 
@@ -64,11 +61,11 @@
                             <!-- Group container for the two buttons -->
                             <div class="d-flex justify-content-between align-items-center">
                                 <button type="reset" class="btn btn-danger text-center">
-                                    Werte zurücksetzen
+                                    {{ __("Reset Values") }}
                                 </button>
 
                                 <button type="submit" class="btn btn-primary text-center">
-                                    Projekt hinzufügen
+                                    {{ __("Add Project") }}
                                 </button>
                             </div>
 
