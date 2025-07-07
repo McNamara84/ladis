d1df97815e5a5a06e50b81fb14d748f46b206221
mukkig <michael-g-0@web.de>
2025-07-07 20:13:20 +0200

    Update UserSeederTest.php
    Defined a method to test whether the BOT user is created when running the UserSeeder.

--- tests/Feature/UserSeederTest.php
+++ tests/Feature/UserSeederTest.php
@@ -2,12 +2,27 @@
 
 namespace Tests\Feature;
 
+use Artisan;
 use Illuminate\Foundation\Testing\RefreshDatabase;
-use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
+use Illuminate\Support\Facades\DB;
 
 class UserSeederTest extends TestCase
 {
+    use RefreshDatabase;
+
+    /**
+     * Test whether the BOT user has been created after calling the seeder.
+     */
+    public function test_user_seeder_creates_expected_records(): void
+    {
+        Artisan::call('db:seed', ['--class' => \Database\Seeders\UserSeeder::class]);
+        $this->assertDatabaseHas('users', [
+            'id' => 1,
+            'name' => 'BOT',
+            'email' => 'bot@example.com',
+        ]);
+    }
     /**
      * A basic feature test example.
      */
