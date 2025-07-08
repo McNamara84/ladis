39dd209440a710f4fed6632139f7377f78ba3df4
mukkig <michael-g-0@web.de>
2025-07-08 14:41:31 +0200

    Update DeviceSeederTest.php
    Changed method completely.
    Now using a valid CSV to verify if the record is created.

--- tests/Feature/DeviceSeederTest.php
+++ tests/Feature/DeviceSeederTest.php
@@ -42,10 +42,29 @@ class DeviceSeederTest extends TestCase
      * Tests whether the number of CSV files matches the number of created device records.
      * Assumes that all relevant CSV files have been correctly generated beforehand.
      */
-    public function test_device_seeder_creates_expected_records(): void
+    public function test_device_seeder_creates_expected_record(): void
     {
+
+        $exampleCsv = <<<CSV
+        name;description;year;build;height;width;depth;weight;fiber_length;cooling;mounting;automation;max_output;mean_output;max_wattage;head;emission_source;beam_type;beam_profile;wavelength;min_spot_size;max_spot_size;min_pf;max_pf;min_pw;max_pw;min_scan_width;max_scan_width;min_focal_length;max_focal_length;safety_class;last_edit_by;institution_id
+        CL 20 BACKPACK;;;;220;400;650;14.5;;0;;;;20;;;;1;;1064;;;;;;;;;;;;1;2
+        CSV;
+
         $fileCount = $this->getFileCount();
-        $this->assertDatabaseCount('devices', $fileCount);
+
+        Storage::disk('local')->put('devices/test.csv', $exampleCsv);
+
+        try {
+
+            Artisan::call('db:seed', [
+                '--class' => \Database\Seeders\DeviceSeeder::class,
+            ]);
+
+            $this->assertDatabaseCount('devices', $fileCount);
+
+        } finally {
+            Storage::disk('local')->delete(['devices/test.csv']);
+        }
     }
     /**
      * Tests that no records are created from non-CSV files.
