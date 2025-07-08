830692e27ebc74dcaa065b6f4dee9e2ea731fcfb
helinkisa <helin.kisa@fh-potsdam.de>
2025-07-08 12:48:30 +0200

    referenced models for 'ProcessInputController'

---
+++ app/Http/Controllers/ProcessInputController.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace App\Http\Controllers;
+use App\Models\Configuration;
+use App\Models\Device;
+use App\Models\PartialSurface;
+use Illuminate\Http\Request;
+use App\Models\Process;
