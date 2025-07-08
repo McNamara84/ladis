efdff5dfae564801371bf32ee0c4764a87698acd
helinkisa <helin.kisa@fh-potsdam.de>
2025-07-08 20:26:14 +0200

    added unit test for input form process
    This test was used to check that an empty input form cannot be saved.

--- tests/Feature/ProcessInputControllerTest.php
+++ tests/Feature/ProcessInputControllerTest.php
@@ -5,16 +5,32 @@ namespace Tests\Feature;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
+use App\Models\PartialSurface;
+use App\Models\Device;
+use App\Models\Configuration;
+use App\Models\Process;
 
 class ProcessInputControllerTest extends TestCase
 {
-    /**
-     * A basic feature test example.
-     */
-    public function test_example(): void
-    {
-        $response = $this->get('/');
+    use RefreshDatabase;
 
-        $response->assertStatus(200);
+    public function test_empty_form_can_not_be_saved(): void
+    {
+        $response = $this->post('/inputform_process', [
+            'partial_surface_id' => '',
+            'device_id' => '',
+            'configuration_id' => '',
+            'description' => '',
+            'duration' => '',
+            'wet' => '',
+        ]);
+
+        $response->assertSessionHasErrors([
+            'partial_surface_id',
+            'device_id',
+            'configuration_id',
+            'duration',
+            'wet',
+        ]);
     }
 }
