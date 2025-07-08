2164dec4fa6ca2725a0e3b69e1d2cafa4ad7c714
mukkig <michael-g-0@web.de>
2025-07-08 18:49:12 +0200

    Update UserSeederTest.php
    Defined method more precisely and made the checks more specific.

--- tests/Feature/UserSeederTest.php
+++ tests/Feature/UserSeederTest.php
@@ -29,15 +29,13 @@ class UserSeederTest extends TestCase
     public function test_user_seeder_does_not_create_bot_user_because_it_already_exists(): void
     {
         Artisan::call('db:seed', ['--class' => \Database\Seeders\UserSeeder::class]);
-        $recordCountBefore = DB::table('users')->count();
         Artisan::call('db:seed', ['--class' => \Database\Seeders\UserSeeder::class]);
-        $recordCountAfter = DB::table('users')->count();
-        $this->assertEquals($recordCountBefore, $recordCountAfter);
-        $this->assertDatabaseHas('users', [
-            'id' => 1,
-            'name' => 'BOT',
-            'email' => 'bot@example.com',
-        ]);
+        
+        $count = DB::table('users')->where('name', 'BOT')->count();
+        $this->assertTrue($count == 1);
 
+        $count = DB::table('users')->where('id', 1)->count();
+        $this->assertTrue($count == 1);
+        
     }
 }
