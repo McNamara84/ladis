0f5f09d03bcaa9ae4450b8bd39ab4ed6c98aa6e6
helinkisa <helin.kisa@fh-potsdam.de>
2025-07-08 12:50:42 +0200

    definition of index method

--- app/Http/Controllers/ProcessInputController.php
+++ app/Http/Controllers/ProcessInputController.php
@@ -6,3 +6,17 @@ use App\Models\Device;
 use App\Models\PartialSurface;
 use Illuminate\Http\Request;
 use App\Models\Process;
+
+class ProcessInputController extends Controller
+{
+    public function index()
+    {
+        $pageTitle = 'Prozesseingabe';
+        // Fetch necessary data for the view, e.g., partial surfaces, devices, configurations
+        $devices = Device::orderBy('name')->get();
+        $partialSurface = PartialSurface::orderBy('sample_surface_id')->get();
+        $configurations = Configuration::orderBy('description')->get();
+
+        return view('inputform_process', compact('pageTitle', 'partialSurface', 'devices', 'configurations'));
+    }   
+
