4aebed56df8bdc55ed1b8c02f81bcad09f5bd38c
McNamara <holgerehrmann@gmail.com>
2025-07-09 07:09:21 +0200

    Revert "Update Eingabemaske link to devices index route"
    This reverts commit 2f27349b13fef7fcf5ec182ca0d4f80205e9cfd8.

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -126,7 +126,7 @@
                                 </a>
                             </li>
                             <li class="nav-item">
-                                <a class="nav-link" href="{{ route('devices.index') }}">
+                                <a class="nav-link" href="{{ route('devices.create') }}">
                                     Eingabemaske
                                 </a>
                             </li>
