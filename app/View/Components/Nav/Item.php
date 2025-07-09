20ed7baa1a86667f7d79e1ccca43cc208b8e7021
nicolaibach <nicolai.bach@mailbox.org>
2025-07-09 10:58:51 +0200

    refactor: Item component to match consumed Link component

--- app/View/Components/Nav/Item.php
+++ app/View/Components/Nav/Item.php
@@ -9,47 +9,31 @@ use Illuminate\View\Component;
 /**
  * Item component for the navigation bar
  *
- * @param string $route The route name
  * @param string $text The display text
- * @param bool $disabled Whether the item is disabled
- * @param string $class Additional CSS classes
+ * @param string|null $route Optional, the route name
+ * @param bool $disabled Optional, whether the item is disabled
+ * @param string $icon Optional ID of an icon from the SVG sprite
+ *
+ * Additional attributes will be passed to the template
  */
 class Item extends Component
 {
     /**
      * Create a new component instance.
      *
-     * @param string $route The route name
      * @param string $text The display text
-     * @param bool $disabled Whether the item is disabled
+     * @param string|null $route Optional, the route name
+     * @param bool $disabled Optional, whether the item is disabled
      * @param string $icon Optional ID of an icon from the SVG sprite
+     *
+     * Additional attributes will be passed to the template
      */
     public function __construct(
-        public string $route,
         public string $text,
+        public ?string $route = null,
         public bool $disabled = false,
         public string $icon = '',
     ) {
-    }
-
-    /**
-     * Get the href for the nav item
-     *
-     * @return string
-     */
-    public function href(): string
-    {
-        return $this->disabled ? '#' : route($this->route);
-    }
-
-    /**
-     * Check if the nav item is active
-     *
-     * @return bool
-     */
-    public function isActive(): bool
-    {
-        return request()->routeIs($this->route);
     }
 
     /**
