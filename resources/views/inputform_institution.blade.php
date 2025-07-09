e15c945c546001b775d23b3028936d8f0e091527
ArturNeufeld <artur.neufeld@fh-potsdam.de>
2025-07-10 00:23:48 +0200

    Update inputform_institution.blade.php

--- resources/views/inputform_institution.blade.php
+++ resources/views/inputform_institution.blade.php
@@ -10,7 +10,7 @@
             <div class="col-md-10">
                 <div class="card">
                     <div class="card-header">
-                        <h4 class="mb-0">Neue Institution hinzufügen</h4>
+                        <h4 class="mb-0">{{ __("Add New Institution") }}</h4>
                     </div>
                     <div class="card-body">
                         {{-- Display validation errors --}}
@@ -36,43 +36,43 @@
                             <div class="row">
                                 <div class="col-md-6">
                                     <div class="form-group mb-3">
-                                        <label for="name" class="form-label">Name der Institution: <strong>*</strong></label>
+                                        <label for="name" class="form-label">{{ __("Name of Institution") }}<strong> *</strong></label>
                                         <input type="text" class="form-control @error('name') is-invalid @enderror"
                                             id="name" name="name" value="{{ old('name') }}" required>
                                         <div class="form-text">
-                                            Bitte geben Sie eine eindeutige Bezeichnung der Institution ein.
+                                            {{ __("messages.k00") }}
                                         </div>
                                     </div>
 
                                     <div class="form-group mb-3">
                                         <label for="type" class="form-label">Typ: <strong>*</strong></label>
                                         <select class="form-control @error('type') is-invalid @enderror" id="type" name="type" required>
-                                            <option value="">Bitte auswählen</option>
-                                            <option value="{{ Institution::TYPE_CLIENT}}" {{ old('type') == Institution::TYPE_CLIENT ? 'selected' : '' }}>Auftraggeber</option>
-                                            <option value="{{ Institution::TYPE_CONTRACTOR}}" {{ old('type') == Institution::TYPE_CONTRACTOR ? 'selected' : '' }}>Auftragnehmer</option>
-                                            <option value="{{ Institution::TYPE_MANUFACTURER}}" {{ old('type') == Institution::TYPE_MANUFACTURER ? 'selected' : '' }}>Hersteller</option>
+                                            <option value="">{{ __("Please select …") }}</option>
+                                            <option value="{{ Institution::TYPE_CLIENT}}" {{ old('type') == Institution::TYPE_CLIENT ? 'selected' : '' }}>{{ __("Client") }}</option>
+                                            <option value="{{ Institution::TYPE_CONTRACTOR}}" {{ old('type') == Institution::TYPE_CONTRACTOR ? 'selected' : '' }}>{{ __("Contractor") }}</option>
+                                            <option value="{{ Institution::TYPE_MANUFACTURER}}" {{ old('type') == Institution::TYPE_MANUFACTURER ? 'selected' : '' }}>{{ __("Manufacturer") }}</option>
                                         </select>
                                         <div class="form-text">
-                                            Bitte wählen Sie die passende Typenbezeichnung aus der Liste aus.
+                                            {{ __("messages.k01") }}
                                         </div>
                                     </div>
 
                                     <div class="form-group mb-3">
-                                        <label for="contact_information" class="form-label">Kontaktinformation: <strong>*</strong></label>
+                                        <label for="contact_information" class="form-label">{{ __("Contact Information") }}<strong> *</strong></label>
                                         <textarea class="form-control @error('contact_information') is-invalid @enderror"
                                             name="contact_information" rows="3" required>{{ old('contact_information') }}</textarea>
                                         <div class="form-text">
-                                            Bitte geben Sie eine Kontaktinformation ein, wie z.B. einen Webseitenlink.
+                                            {{ __("messages.k02") }}
                                         </div>
                                     </div>
                                     <div class="form-text mb-3">
-                                        <strong>*</strong> Pflichtangabe
+                                        <strong>* </strong>{{ __("Mandatory Field") }}
                                     </div>
                                 </div>
                             </div>
                             <div class="form-group mt-4">
-                                <button type="reset" class="btn btn-secondary">Werte zurücksetzen</button>
-                                <button type="submit" class="btn btn-primary">Hinzufügen</button>
+                                <button type="reset" class="btn btn-secondary">{{ __("Reset Values") }}</button>
+                                <button type="submit" class="btn btn-primary">{{ __("Submit") }}</button>
                             </div>
                         </form>
                     </div>
