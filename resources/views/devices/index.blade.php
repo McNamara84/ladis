50d8611575eb2bf77a87d31ca011ff13a2079c96
ArturNeufeld <artur.neufeld@fh-potsdam.de>
2025-07-10 12:04:37 +0200

    Update index.blade.php

--- resources/views/devices/index.blade.php
+++ resources/views/devices/index.blade.php
@@ -1,23 +1,23 @@
 @extends('layouts.app')
 
-@section('title', 'Geräte')
+@section('title', __('Devices'))
 
 @section('content')
     <div class="container">
-        <h1 class="h3 mb-4">Alle Lasergeräte</h1>
+        <h1 class="h3 mb-4">{{ __("All Laser Devices") }}</h1>
         <div class="table-responsive">
             <table class="table table-striped">
                 <thead>
                     <tr>
-                        <th>ID</th>
-                        <th>Name</th>
-                        <th>Institution</th>
-                        <th>Baujahr</th>
-                        <th>Bauart</th>
-                        <th>Strahltyp</th>
-                        <th>Max. Leistung (W)</th>
+                        <th>{{ __("ID") }}</th>
+                        <th>{{ __("Name") }}</th>
+                        <th>{{ __("Institution") }}</th>
+                        <th>{{ __("Build Year") }}</th>
+                        <th>{{ __("Build Type") }}</th>
+                        <th>{{ __("Beam Profile") }}</th>
+                        <th>{{ __("Max. Power") }} (in W)</th>
                         @auth
-                            <th>Aktionen</th>
+                            <th>{{ __("Actions") }}</th>
                         @endauth
                     </tr>
                 </thead>
@@ -34,25 +34,25 @@
                             @auth
                                 <td>
                                     <button type="button" class="btn btn-danger btn-sm" data-bs-toggle="modal" data-bs-target="#deleteDevice{{ $device->id }}">
-                                        Löschen
+                                        {{ __("Delete") }}
                                     </button>
 
                                     <div class="modal fade" id="deleteDevice{{ $device->id }}" tabindex="-1" aria-labelledby="deleteDevice{{ $device->id }}Label" aria-hidden="true">
                                         <div class="modal-dialog">
                                             <div class="modal-content">
                                                 <div class="modal-header">
-                                                    <h5 class="modal-title" id="deleteDevice{{ $device->id }}Label">Gerät löschen</h5>
-                                                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Schließen"></button>
+                                                    <h5 class="modal-title" id="deleteDevice{{ $device->id }}Label">{{ __("Delete Device") }}</h5>
+                                                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="{{ __("Close") }}"></button>
                                                 </div>
                                                 <div class="modal-body">
-                                                    Soll das Gerät <strong>{{ $device->name }}</strong> wirklich gelöscht werden?
+                                                    {!! __("messages.m00", [":deviceName" => $device->name]) !!}
                                                 </div>
                                                 <div class="modal-footer">
-                                                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Abbrechen</button>
+                                                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">{{ __("Cancel") }}</button>
                                                     <form method="POST" action="{{ route('devices.destroy', $device->id) }}" class="d-inline">
                                                         @csrf
                                                         @method('DELETE')
-                                                        <button type="submit" class="btn btn-danger">Löschen</button>
+                                                        <button type="submit" class="btn btn-danger">{{ __("Delete") }}</button>
                                                     </form>
                                                 </div>
                                             </div>
@@ -63,14 +63,14 @@
                         </tr>
                     @empty
                         <tr>
-                            <td colspan="@auth 8 @else 7 @endauth">Keine Geräte vorhanden.</td>
+                            <td colspan="@auth 8 @else 7 @endauth">{{ __("No devices available.") }}</td>
                         </tr>
                     @endforelse
                 </tbody>
             </table>
         </div>
         @auth
-            <a href="{{ url('/devices/create') }}" class="btn btn-primary mt-3">Lasergerät hinzufügen</a>
+            <a href="{{ url('/devices/create') }}" class="btn btn-primary mt-3">{{ __("Add Laser Device") }}</a>
         @endauth
     </div>
 @endsection
