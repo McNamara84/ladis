c4b6bd1357830dd41700ef3fe3a1fb2839f8f67e
Maria-Schendel <149330325+Maria-Schendel@users.noreply.github.com>
2025-07-10 10:50:25 +0200

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -123,3 +123,7 @@ Route::middleware('auth')->group(function () {
     Route::post('/inputform_material', [MaterialInputController::class, 'store'])->name('inputform_material.store');
 
 });
+
+
+// Project management
+Route::get('/inputform_project', [ProjectInputController::class, 'index'])->name('projects.index');
