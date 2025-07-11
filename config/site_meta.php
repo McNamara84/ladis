7dfab1dfa3b7ed89e3cb8638349a545f2ed5ee85
nicolaibach <nicolai.bach@mailbox.org>
2025-07-11 22:46:08 +0200

    feat: add basic site_meta config

---
+++ config/site_meta.php
@@ -0,0 +1,29 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Site Meta Data
+|--------------------------------------------------------------------------
+|
+| The settings in this file are used to configure the site's metadata.
+|
+| They affect the generation of the sites metadata (`<head>`) and control
+| the bits and pieces of information that's displayed in common locations
+| like page headers and footers.
+|
+*/
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Primary Contact Information
+    |--------------------------------------------------------------------------
+    |
+    | This is the primary contact information for the website.
+    |
+    */
+
+    'contact' => env('SITE_CONTACT', 'site_info.contact_fhp'),
+
+];
