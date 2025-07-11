a7b9271968348b8c299cfc429bcc57a50c6705c6
McNamara <holgerehrmann@gmail.com>
2025-07-11 17:48:28 +0200

    Set model property in ConditionFactory
    Added the protected $model property to ConditionFactory to explicitly define the associated model as Condition. This improves clarity and ensures correct factory behavior.

--- database/factories/ConditionFactory.php
+++ database/factories/ConditionFactory.php
@@ -4,17 +4,15 @@ namespace Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
 use App\Models\DamagePattern;
+use App\Models\Condition;
 
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Condition>
  */
 class ConditionFactory extends Factory
 {
-    /**
-     * Define the model's default state.
-     *
-     * @return array<string, mixed>
-     */
+    protected $model = Condition::class;
+
     public function definition(): array
     {
         return [
