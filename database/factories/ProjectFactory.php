ec5cbb1f68d9c5a4752658b45a2cff1cab9dc28f
McNamara <holgerehrmann@gmail.com>
2025-07-12 04:14:29 +0200

    Add factory for Project model
    Introduces ProjectFactory to generate test data for the Project model, including related person and venue associations.

---
+++ database/factories/ProjectFactory.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use App\Models\Person;
+use App\Models\Venue;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Project>
+ */
+class ProjectFactory extends Factory
+{
+    public function definition(): array
+    {
+        return [
+            'name' => $this->faker->unique()->words(3, true),
+            'description' => $this->faker->sentence(),
+            'url' => $this->faker->unique()->url(),
+            'started_at' => $this->faker->date(),
+            'ended_at' => $this->faker->date(),
+            'person_id' => Person::factory(),
+            'venue_id' => Venue::factory(),
+        ];
+    }
+}
