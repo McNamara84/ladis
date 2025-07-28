6bd97ad0259b12b6ac853e9f1a1ade9fcf9f28fb
nicolaibach <nicolai.bach@mailbox.org>
2025-07-28 20:55:04 +0200

    feat: privacy page uses contact service and view components

--- app/Http/Controllers/PrivacyPolicyController.php
+++ app/Http/Controllers/PrivacyPolicyController.php
@@ -2,14 +2,30 @@
 
 namespace App\Http\Controllers;
 
+use App\Services\Contacts\ContactsService;
+
 // This controller handles the display of the Privacy Policy page.
 class PrivacyPolicyController extends Controller
 {
-    public function index()
+    public function index(ContactsService $contactsService)
     {
         $pageTitle = 'Datenschutzerklärung';
         $lastUpdated = '2025-06-21T00:00:00Z';
 
-        return view('privacy-policy', ['pageTitle' => $pageTitle, 'lastUpdated' => $lastUpdated]);
+        $contactResponsible = $contactsService->{config('site.contact.responsible')};
+        $contactPrivacy = $contactsService->{config('site.contact.privacy')};
+        $contactHosting = $contactsService->{config('site.contact.hosting')};
+        $contactLdabb = $contactsService->ldabb;
+        $contactFhpKommunikation = $contactsService->{'fhp-kommunikation'};
+
+        return view('privacy-policy', compact(
+            'pageTitle',
+            'lastUpdated',
+            'contactResponsible',
+            'contactPrivacy',
+            'contactHosting',
+            'contactLdabb',
+            'contactFhpKommunikation',
+        ));
     }
 }
