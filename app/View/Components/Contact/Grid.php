d87e501e0825f7a267a60e986d914d567af3782f
nicolaibach <nicolai.bach@mailbox.org>
2025-08-02 10:13:20 +0200

    refactor: update CardList to use the unified Contact view component
    - accepts `$variant` attribute and passes it down to `Contact` components
    - renamed to `Grid`
        - `CardList` is not appropriate anymore
        - just `List` is not a valid name for the class
        - settled on `Grid` because – although a `ul` is used – the layout behaves like a grid with rows and columns

--- app/View/Components/Contact/Grid.php
+++ app/View/Components/Contact/Grid.php
@@ -9,13 +9,13 @@ use App\View\Components\Contact\Contact;
 use App\Services\Contacts\Models\Contact as ContactModel;
 
 /**
- * Contact Card List component
+ * Contact Grid component
  *
- * Renders a list of contact cards.
+ * Renders a responsive grid of contact cards.
  *
  * @since 0.2.0
  */
-class CardList extends Component
+class Grid extends Component
 {
 
     /**
@@ -24,11 +24,13 @@ class CardList extends Component
      * @param array<string, ContactModel> $contacts The contacts to render
      * @param string $nameFormat Optional. The format of the name
      * @param string $headingLevel Optional. The heading level for the contact name
+     * @param string $variant Optional. The layout variant for the contact
      */
     public function __construct(
         public array $contacts,
         public string $nameFormat = ContactModel::DEFAULT_NAME_FORMAT,
         public string $headingLevel = Contact::DEFAULT_HEADING_LEVEL,
+        public string $variant = Contact::DEFAULT_VARIANT,
     ) {
     }
 
@@ -37,7 +39,7 @@ class CardList extends Component
      */
     public function render(): View|Closure|string
     {
-        return view('components.contact.card-list');
+        return view('components.contact.grid');
     }
 
 }
--- resources/views/components/contact/grid.blade.php
+++ resources/views/components/contact/grid.blade.php
@@ -1,7 +1,7 @@
 <ul class="list-unstyled row row-cols-1 row-cols-lg-2 row-cols-xl-3 g-5 card-list" role="list">
     @foreach($contacts as $contact)
         <li class="col">
-            <x-contact.card :contact="$contact" :name-format="$nameFormat" :heading-level="$headingLevel" />
+            <x-contact :contact="$contact" :name-format="$nameFormat" :heading-level="$headingLevel" :variant="$variant" />
         </li>
     @endforeach
 </ul>
