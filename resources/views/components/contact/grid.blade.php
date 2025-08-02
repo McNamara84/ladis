48e32c35791e27047d8872fac88a6409872124ea
nicolaibach <nicolai.bach@mailbox.org>
2025-08-02 20:33:01 +0200

    feat: contact grid view handles custom attributes

--- resources/views/components/contact/grid.blade.php
+++ resources/views/components/contact/grid.blade.php
@@ -1,4 +1,6 @@
-<ul class="list-unstyled row row-cols-1 row-cols-lg-2 row-cols-xl-3 g-5 {{ $variant }}-grid" role="list">
+<ul {{ $attributes
+    ->class(['list-unstyled', 'row', 'row-cols-1', 'row-cols-lg-2', 'row-cols-xl-3', 'g-5', "$variant-grid"])
+    ->merge(['role' => 'list']) }}>
     @foreach($contacts as $contact)
         <li class="col">
             <x-contact :contact="$contact" :name-format="$nameFormat" :heading-level="$headingLevel" :variant="$variant" />
