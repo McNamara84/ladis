a82d32396cf1bd9ae330d593b0ad1bfaa5d645bd
nicolaibach <nicolai.bach@mailbox.org>
2025-08-03 08:20:20 +0200

    fix: remove unnecessary imports in AboutPageTest

--- tests/Feature/AboutPageTest.php
+++ tests/Feature/AboutPageTest.php
@@ -2,15 +2,11 @@
 
 namespace Tests\Feature;
 
-use Illuminate\Foundation\Testing\RefreshDatabase;
-use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 
 class AboutPageTest extends TestCase
 {
-    use RefreshDatabase;
-
-    public function test_about_page_returns_successful_response(): void
+    public function test_about_page_response_is_ok(): void
     {
         $response = $this->get('/ladis');
 
