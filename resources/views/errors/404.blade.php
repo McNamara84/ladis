faf15bd2b4a4de8f00835c6001db834f51274e4b
McNamara <holgerehrmann@gmail.com>
2025-08-03 19:49:11 +0200

    Refactor error pages to use shared CSS file
    Moved inline styles from error Blade templates (404, 419, 500, 502) to a new public/css/error.css file for consistency and maintainability. Updated image and attribution markup to use CSS classes.

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -3,14 +3,12 @@
 <head>
     <meta charset="UTF-8">
     <title>404 Not Found</title>
-    <style>
-        body { text-align: center; font-family: sans-serif; margin-top: 50px; }
-    </style>
+    <link rel="stylesheet" href="{{ asset('css/error.css') }}">
 </head>
 <body>
     <h1>404 Not Found</h1>
-    <img src="https://live.staticflickr.com/5237/14088999339_e37c26afa6_b.jpg" alt="404 Cat" style="max-width:300px; border-radius:12px;">
-    <p class="attribution">"<a rel="noopener noreferrer" href="https://www.flickr.com/photos/26344495@N05/14088999339">Katze gähnt wie ein kleiner Tiger</a>" by <a rel="noopener noreferrer" href="https://www.flickr.com/photos/26344495@N05">Ivan Radic</a> is licensed under <a rel="noopener noreferrer" href="https://creativecommons.org/licenses/by/2.0/?ref=openverse">CC BY 2.0 <img src="https://mirrors.creativecommons.org/presskit/icons/cc.svg" style="height: 1em; margin-right: 0.125em; display: inline;" /><img src="https://mirrors.creativecommons.org/presskit/icons/by.svg" style="height: 1em; margin-right: 0.125em; display: inline;" /></a>.</p>
+    <img src="https://live.staticflickr.com/5237/14088999339_e37c26afa6_b.jpg" alt="404 Cat" class="error-image">
+    <p class="attribution">"<a rel="noopener noreferrer" href="https://www.flickr.com/photos/26344495@N05/14088999339">Katze gähnt wie ein kleiner Tiger</a>" by <a rel="noopener noreferrer" href="https://www.flickr.com/photos/26344495@N05">Ivan Radic</a> is licensed under <a rel="noopener noreferrer" href="https://creativecommons.org/licenses/by/2.0/?ref=openverse">CC BY 2.0 <img src="https://mirrors.creativecommons.org/presskit/icons/cc.svg" /><img src="https://mirrors.creativecommons.org/presskit/icons/by.svg" /></a>.</p>
     <p>Sorry, this page could not be found.</p>
 </body>
 </html>
