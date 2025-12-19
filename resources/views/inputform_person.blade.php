e6916edd2eadde8377cf31aa0d642c11ba0abcef
McNamara <holgerehrmann@gmail.com>
2025-12-19 20:15:44 +0100

    Add reusable card form component and refactor person form
    Introduced a new Blade component 'card.form' for reusable card-styled forms. Refactored the person input form to use this new component, simplifying the markup and improving consistency.

--- resources/views/inputform_person.blade.php
+++ resources/views/inputform_person.blade.php
@@ -1,47 +1,36 @@
 @extends('layouts.app')
 @section('title', $pageTitle)
 @section('content')
-    <div class="container">
-        <div class="row justify-content-center">
-            <div class="col-md-10">
-                <div class="card">
-                    <div class="card-header">
-                        <h4 class="mb-0">Neue Person hinzufügen</h4>
-                    </div>
-                    <div class="card-body">
-                        <x-form.alerts />
+    <x-card.form title="Neue Person hinzufügen">
+        <x-form.alerts />
 
-                        <form action="{{ route('inputform_person.store') }}" method="post">
-                            @csrf
-                            <div class="row">
-                                <div class="col-md-6">
-                                    <x-form.input
-                                        name="name"
-                                        label="Name"
-                                        :required="true"
-                                    />
+        <form action="{{ route('inputform_person.store') }}" method="post">
+            @csrf
+            <div class="row">
+                <div class="col-md-6">
+                    <x-form.input
+                        name="name"
+                        label="Name"
+                        :required="true"
+                    />
 
-                                    <x-form.select
-                                        name="institution_id"
-                                        label="Institution"
-                                        :options="$institutions"
-                                        placeholder="Bitte auswählen"
-                                        :required="true"
-                                    />
+                    <x-form.select
+                        name="institution_id"
+                        label="Institution"
+                        :options="$institutions"
+                        placeholder="Bitte auswählen"
+                        :required="true"
+                    />
 
-                                    <div class="form-text mb-3">
-                                        <strong>*</strong> Pflichtangabe
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="form-group mt-4">
-                                <button type="reset" class="btn btn-secondary">Werte zurücksetzen</button>
-                                <button type="submit" class="btn btn-primary">Hinzufügen</button>
-                            </div>
-                        </form>
+                    <div class="form-text mb-3">
+                        <strong>*</strong> Pflichtangabe
                     </div>
                 </div>
             </div>
-        </div>
-    </div>
+            <div class="form-group mt-4">
+                <button type="reset" class="btn btn-secondary">Werte zurücksetzen</button>
+                <button type="submit" class="btn btn-primary">Hinzufügen</button>
+            </div>
+        </form>
+    </x-card.form>
 @endsection
